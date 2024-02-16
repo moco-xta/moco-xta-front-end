63fd3a41ddf4e95ac9a63814dc047e7c061058c4
moco-xta <moco.xta@gmail.com>
2024-02-16 16:54:49 +0100

    glabal.scss cleaned + routes created to replace navMenu.ts + variables.module.scss created + HamburgerMenuIcon svg component + Desktop nav & mobile sidebar menu

---
+++ src/routes/routes.ts
@@ -0,0 +1,26 @@
+export const Routes = [
+  {
+    name: 'Home',
+    path: '/',
+  },
+  {
+    name: 'About',
+    path: '/about',
+  },
+  {
+    name: 'Blog',
+    path: '/blog',
+  },
+  {
+    name: 'Contact',
+    path: '/contact',
+  },
+  {
+    name: 'Photography',
+    path: '/photography',
+  },
+  {
+    name: 'Projects',
+    path: '/projects',
+  },
+]
