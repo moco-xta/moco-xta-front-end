a18ee9feb1e3de72c690766b6113645718b0ec96
moco-xta <moco.xta@gmail.com>
2024-02-16 18:56:45 +0100

    next.config.mjs updated for glsl + glsl.d.ts file created to fix glsl files import + DefaultScene with shader material & external vertextShader/fragmentShader + r3f default scene removed

--- src/slices/hero/index.tsx
+++ src/slices/hero/index.tsx
@@ -1,46 +1,9 @@
-'use client'
+import React from 'react'
 
-import React, { useRef, useState } from 'react'
-import * as THREE from 'three'
-import { Canvas, useFrame } from '@react-three/fiber'
+import DefaultScene from '@/components/r3f/scenes/default_scene'
 
 import './index.scss'
 
-interface BoxInterface {
-  position: THREE.Vector3
-}
-
-function Box({ position }: BoxInterface) {
-  const meshRef = useRef(null)
-  
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-
-  // @ts-ignore
-  useFrame((state, delta) => (meshRef.current.rotation.x += delta))
-
-  return (
-    <mesh
-      ref={meshRef}
-      position={position}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
-}
-
 export default function Hero() {
-  return (
-    <Canvas>
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={new THREE.Vector3(-1.2, 0, 0)} />
-      <Box position={new THREE.Vector3(1.2, 0, 0)} />
-    </Canvas>
-  )
+  return <DefaultScene />
 }
