90b112176a83c90fbcc549fa8bead32a8cc7dbac
moco-xta <moco.xta@gmail.com>
2024-02-17 12:10:57 +0100

    Redux set up: store + ReduxProvider (--> layout.tsx) + apiSlice + testApi + ApiTest slice + testInterface + shortcuts rewrite in tsconfig.json + apiConstants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
-import Hero from '@/slices/hero'
+import Hero from '@/components/slices/hero'
+import ApiTest from '@/components/slices/api_test'
 
 export default function Home() {
-  return <Hero />
+  return (
+    <>
+      <Hero />
+      <ApiTest />
+    </>
+  )
 }
---
+++ src/components/slices/api_test/index.tsx
@@ -0,0 +1,77 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+
+import { useGetTestsQuery, useAddNewTestMutation } from '@/redux/api/testApi'
+import { TestInterface } from '@/interfaces/api/testInterface'
+
+export default function ApiTest() {
+
+  const [title, setTitle] = useState<string>('')
+  const [description, setDescription] = useState<string>('')
+  const [dateOfCreation, setDateOfCreation] = useState<string>('1995-12-17')
+  const [category, setCategory] = useState<string>('')
+
+  useEffect(() => {
+    console.log('dateOfCreation', dateOfCreation)
+  }, [dateOfCreation])
+
+  const { data, isLoading, error } = useGetTestsQuery()
+  const [addNewTest] = useAddNewTestMutation()
+
+  let content = null
+
+  if (isLoading) content = <div>Is loading...</div>
+
+  if (error) content = <div>An error occured...</div>
+
+  if (data) {
+    content = data?.map((task, index) => (
+      <div key={`task${index}`}>{task.title}</div>
+    ))
+  }
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault()
+    const test: TestInterface = {
+      title: title,
+      description: description,
+      date_of_creation: new Date(dateOfCreation),
+    }
+
+    console.log('test', test)
+    addNewTest(test)
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          id='title'
+          type='text'
+          placeholder='Title'
+          name='title'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          id='description'
+          type='text'
+          placeholder='Description'
+          name='description'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <input
+          id='date_of_creation'
+          type='date'
+          name='date_of_creation'
+          value={dateOfCreation}
+          onChange={(e) => setDateOfCreation(e.target.value)}
+        />
+        <button type='submit'>Submit</button>
+      </form>
+      {content}
+    </>
+  )
+}
