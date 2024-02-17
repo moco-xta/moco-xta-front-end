2d4a7faadae66540286566f0b818cfda988ecabb
moco-xta <moco.xta@gmail.com>
2024-02-17 12:10:57 +0100

    Redux set up: store + ReduxProvider (--> layout.tsx) + apiSlice + testApi + ApiTest slice + testInterface + shortcuts rewrite in tsconfig.json + apiConstants

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.scss'
+
+import ReduxProvider from '@/redux/ReduxProvider'
+
 import Header from '@/components/shared/header'
+
+import './globals.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,8 +22,10 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <Header />
-        {children}
+        <ReduxProvider>
+          <Header />
+          {children}
+        </ReduxProvider>
       </body>
     </html>
   )
---
+++ src/interfaces/api/testInterface.ts
@@ -0,0 +1,6 @@
+export interface TestInterface {
+  id?: number
+  title: string
+  date_of_creation: Date
+  description: string
+}
