68c417c740816058ab8570f372d52e4742091673
moco-xta <moco.xta@gmail.com>
2024-02-20 23:57:56 +0100

    IconChatGpt

---
+++ src/components/r3f/models/icons/IconChatGpt.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useGLTF } from '@react-three/drei'
+
+import { default as GltfConstants } from '@/constants/gltfConstants.json'
+
+export function IconChatGpt(props) {
+  const { nodes, materials } = useGLTF(GltfConstants.ICON_CHAT_GPT)
+  return (
+    <group
+      {...props}
+      dispose={null}
+      position={[-3, 0, 0]}
+    >
+      <mesh
+        geometry={nodes.IconChatGpt.geometry}
+        material={materials['icon_chat_gpt_#75ab9d']}
+        receiveShadow
+        castShadow
+      />
+    </group>
+  )
+}
+
+useGLTF.preload(GltfConstants.ICON_CHAT_GPT)
