e86ec58d0b2c2570f4b0034e67df294018d439b1
moco-xta <moco.xta@gmail.com>
2024-02-20 20:34:09 +0100

    PerformedWithScene created as default and imported in PerformWith slice + issue fixed with canvas's sizes

--- src/components/slices/hero/index.tsx
+++ src/components/slices/hero/index.tsx
@@ -5,5 +5,9 @@ import DefaultScene from '@/components/r3f/scenes/default_scene'
 import './index.scss'
 
 export default function Hero() {
-  return <DefaultScene />
+  return (
+    <div id='default_scene_container'>
+      <DefaultScene />
+    </div>
+  )
 }
