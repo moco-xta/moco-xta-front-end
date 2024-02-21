46cfcbbf57709ccb23906a7438d7031920dcf02a
moco-xta <moco.xta@gmail.com>
2024-02-21 11:39:56 +0100

    IconNextjs

---
+++ src/components/r3f/models/icons/IconNextjs.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useGLTF } from '@react-three/drei'
+
+import { default as GltfConstants } from '@/constants/gltfConstants.json'
+
+export function IconNextjs(props) {
+  const { nodes, materials } = useGLTF(GltfConstants.ICON_NEXTJS)
+  return (
+    <group
+      {...props}
+      dispose={null}
+      position={[-12, 0, 0]}
+    >
+      <mesh
+        geometry={nodes.IconNextjs.geometry}
+        material={materials['icon_nextjs_#ffffff']}
+      />
+    </group>
+  )
+}
+
+useGLTF.preload(GltfConstants.ICON_NEXTJS)
