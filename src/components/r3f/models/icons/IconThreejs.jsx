0288200a18eca67bf0c907bcd19594a2b2c8de92
moco-xta <moco.xta@gmail.com>
2024-02-21 17:05:09 +0100

    IconThreejs

---
+++ src/components/r3f/models/icons/IconThreejs.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useGLTF } from '@react-three/drei'
+
+import { default as GltfConstants } from '@/constants/gltfConstants.json'
+
+export function IconThreejs(props) {
+  const { nodes, materials } = useGLTF(GltfConstants.ICON_THREEJS)
+  return (
+    <group
+      {...props}
+      dispose={null}
+      position={[4.5, 0, 0]}
+    >
+      <group rotation={[Math.PI / 2, 0, 0]}>
+        <mesh
+          geometry={nodes.IconThreejs_1.geometry}
+          material={materials['icon_threejs_#ffffff']}
+          receiveShadow
+          castShadow
+        />
+        <mesh
+          geometry={nodes.IconThreejs_2.geometry}
+          material={materials['icon_threejs_#000000']}
+          receiveShadow
+          castShadow
+        />
+      </group>
+    </group>
+  )
+}
+
+useGLTF.preload(GltfConstants.ICON_THREEJS)
