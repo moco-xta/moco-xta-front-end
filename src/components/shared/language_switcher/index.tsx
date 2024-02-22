e6022c561a2c1ba52aac3926dc775c4bce1746d0
moco-xta <moco.xta@gmail.com>
2024-02-22 17:28:26 +0100

    Issue internationalized routing: LangaugeSwitcher select added with list of locales

--- src/components/shared/language_switcher/index.tsx
+++ src/components/shared/language_switcher/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import { default as LocalesConstants } from '@/constants/localesConstants.json'
+
 import './index.scss'
 
 export default function LangaugeSwitcher() {
   return (
-    <div>LangaugeSwitcher</div>
+    <div>
+      <select>
+        {LocalesConstants.LOCALES.map(locale => (
+          <option value={locale}>{locale}</option>
+        ))}
+      </select>
+    </div>
   )
 }
