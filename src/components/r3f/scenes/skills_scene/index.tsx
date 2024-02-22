b6ccafdc5b6467d4f6de8a21971f0d4bdbf12c1d
moco-xta <moco.xta@gmail.com>
2024-02-22 22:04:59 +0100

    npm install --save @theatre/core @theatre/studio @theatre/r3f --save + Default setup: camera, cube and light

--- src/components/r3f/scenes/skills_scene/index.tsx
+++ src/components/r3f/scenes/skills_scene/index.tsx
@@ -2,31 +2,28 @@
 
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { getProject } from '@theatre/core'
+import { editable as e, PerspectiveCamera, SheetProvider } from '@theatre/r3f'
+import studio from '@theatre/studio'
 
-function Box() {
-  return (
-    <mesh>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={'hotpink'} />
-    </mesh>
-  )
-}
+studio.initialize()
+
+const demoSheet = getProject('Demo Project').sheet('Demo Sheet')
 
 export default function SkillsScene() {
   return (
-    <Canvas
-      shadows
-      legacy
-      gl={{
-        antialias: true,
-        alpha: true,
-        powerPreference: 'high-performance',
-      }}
-    >
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box />
+    <Canvas>
+      <SheetProvider sheet={getProject('Demo Project').sheet('Demo Sheet')}>
+        <PerspectiveCamera theatreKey="Camera" makeDefault position={[5, 5, -5]} fov={75} />
+        <ambientLight />
+        <e.pointLight theatreKey="Light" position={[10, 10, 10]} />
+        <e.mesh theatreKey="Cube">
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="orange" />
+        </e.mesh>
+      </SheetProvider>
+      <OrbitControls />
     </Canvas>
   )
 }
