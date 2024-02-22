9d902536d742c6014c44f9f569deaa026f63a407
moco-xta <moco.xta@gmail.com>
2024-02-22 21:22:29 +0100

    SkillsScene with default cube

--- src/app/[locale]/skills/page.tsx
+++ src/app/[locale]/skills/page.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
+import SkillsScene from '@/components/r3f/scenes/skills_scene'
+
+import './index.scss'
+
 export default function Skills() {
-  return <div>Skills</div>
+  return (
+    <div id='skills_scene_container'>
+      <SkillsScene />
+    </div>
+  )
 }
