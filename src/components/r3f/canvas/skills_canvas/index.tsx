81aeebf74d75e860cc49b579e472c5f3dac23243
moco-xta <moco.xta@gmail.com>
2024-02-23 12:13:11 +0100

    npx prettier --write .

--- src/components/r3f/canvas/skills_canvas/index.tsx
+++ src/components/r3f/canvas/skills_canvas/index.tsx
@@ -31,9 +31,7 @@ function SkillsScene() {
         far={70}
       />
       <ambientLight />
-      <pointLight
-        position={[10, 10, 10]}
-      />
+      <pointLight position={[10, 10, 10]} />
       <mesh>
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color='orange' />
