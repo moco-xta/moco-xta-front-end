6dc955ed0b2dde75e113f42a0d8cfda46ddb46cf
moco-xta <moco.xta@gmail.com>
2024-02-24 12:40:21 +0100

    NavBar: li's positions fixed by languages + SideBarmenu improved BUT still need work

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'MOCO.xta',
-  description: 'Moco\'s portfolio',
+  description: "Moco's portfolio",
 }
 
 export default function RootLayout({
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,29 +4,64 @@ export const Routes: RoutesInterface[] = [
   {
     key: 'HOME',
     path: '/',
+    width: {
+      en: '',
+      es: '',
+      fr: '',
+    }
   },
   {
     key: 'ABOUT',
     path: '/about',
+    width: {
+      en: '76px',
+      es: '90px',
+      fr: '74px',
+    }
   },
   {
     key: 'BLOG',
     path: '/blog',
+    width: {
+      en: '70px',
+      es: '66px',
+      fr: '70px',
+    }
   },
   {
     key: 'CONTACT',
     path: '/contact',
+    width: {
+      en: '86px',
+      es: '72px',
+      fr: '64px',
+    }
   },
   {
     key: 'PHOTOGRAPHY',
     path: '/photography',
+    width: {
+      en: '136px',
+      es: '110px',
+      fr: '136px',
+    }
   },
   {
     key: 'PROJECTS',
     path: '/projects',
+    width: {
+      en: '86px',
+      es: '82px',
+      fr: '56px',
+    }
   },
   {
     key: 'SKILLS',
     path: '/skills',
+    width: {
+      en: '80px',
+      es: '120px',
+      fr: '142px',
+    }
   },
 ]
