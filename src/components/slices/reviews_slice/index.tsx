243640d12ca63616a3b60a7a9f023bbf6f5bbb55
moco-xta <moco.xta@gmail.com>
2024-02-26 23:25:03 +0100

    routes: restructured with RouteInterface modified -> navBar updated + link aded in ReviewsSlice to /add_review

--- src/components/slices/reviews_slice/index.tsx
+++ src/components/slices/reviews_slice/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Link from 'next/link'
+
+import { Routes } from '@/routes/routes'
 
 import './index.scss'
 
 export default function ReviewsSlice() {
   return  (
-    <div id='reviews_slice'>Reviews</div>
+    <div id='reviews_slice'>
+      <p>Reviews</p>
+      <Link href={Routes.find((route) => route.key === 'ADD_REVIEW')!.path}>
+        Add review
+      </Link>
+    </div>
   )
 }
