06b8b207dcc64965eec41446edb3db072cac254e
moco-xta <moco.xta@gmail.com>
2024-02-26 22:56:50 +0100

    ReviewsSlice: index.tsx (rfc) & index.scss + added to home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 
-import Hero from '@/components/slices/hero'
-import Introduction from '@/components/slices/introduction'
-import PerformedWith from '@/components/slices/performed_with'
+import HeroSlice from '@/components/slices/hero_slice'
+import IntroductionSlice from '@/components/slices/introduction_slice'
+import PerformedWithSlice from '@/components/slices/performed_with_slice'
+import ReviewsSlice from '@/components/slices/reviews_slice'
 
 import './index.scss'
 
 export default function Home() {
   return (
     <div id='home_container'>
-      <Hero />
-      <Introduction />
-      <PerformedWith />
+      <HeroSlice />
+      <IntroductionSlice />
+      <ReviewsSlice />
+      <PerformedWithSlice />
     </div>
   )
 }
