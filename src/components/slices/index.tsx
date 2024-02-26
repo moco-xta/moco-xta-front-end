9d07db025abf2ea9ec7c8e7cbcf31497ea9c1689
moco-xta <moco.xta@gmail.com>
2024-02-26 22:59:38 +0100

    index added to slices to simplify import in home page

---
+++ src/components/slices/index.tsx
@@ -0,0 +1,11 @@
+import HeroSlice from '@/components/slices/hero_slice'
+import IntroductionSlice from '@/components/slices/introduction_slice'
+import PerformedWithSlice from '@/components/slices/performed_with_slice'
+import ReviewsSlice from '@/components/slices/reviews_slice'
+
+export {
+  HeroSlice,
+  IntroductionSlice,
+  PerformedWithSlice,
+  ReviewsSlice
+}
