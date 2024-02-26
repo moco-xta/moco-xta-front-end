0127c61994d961013620de123c0970105cc374a7
moco-xta <moco.xta@gmail.com>
2024-02-26 23:12:13 +0100

    reviewApi + reviewInterface + review added to apiConstants & apiSlice

---
+++ src/redux/api/reviewApi.ts
@@ -0,0 +1,24 @@
+import apiSlice from '../slice/apiSlice'
+
+import { default as ApiConstants } from '@/constants/apiConstants.json'
+
+import { ReviewInterface } from '@/interfaces/api/reviewInterface'
+
+const reviewApi = apiSlice.injectEndpoints({
+  endpoints: (build) => ({
+    getReviews: build.query<ReviewInterface[], void>({
+      query: () => ApiConstants.REVIEW,
+      providesTags: ['Review'],
+    }),
+    addNewReview: build.mutation<ReviewInterface, ReviewInterface>({
+      query: (review) => ({
+        url: ApiConstants.REVIEW,
+        method: 'POST',
+        body: review,
+      }),
+      invalidatesTags: ['Review'],
+    }),
+  }),
+})
+
+export const { useGetReviewsQuery, useAddNewReviewMutation } = reviewApi
