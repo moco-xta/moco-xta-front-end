b9550c5d5a236d57a49a621e906432b124d543fc
moco-xta <moco.xta@gmail.com>
2024-02-28 01:54:07 +0100

    Reviews: useGetReviewsQuery() NO css

--- src/app/[locale]/reviews/page.tsx
+++ src/app/[locale]/reviews/page.tsx
@@ -1,7 +1,37 @@
+'use client'
+
 import React from 'react'
 
+import { useGetReviewsQuery } from '@/redux/api/reviewApi'
+
+import ReviewCard from '@/components/cards/review_card'
+
 export default function Reviews() {
+  const { data, isLoading, error } = useGetReviewsQuery()
+
+  let content = null
+
+  if (isLoading) content = <div>Is loading...</div>
+
+  if (error) content = <div>An error occured...</div>
+
+  if (data)
+    content = (
+      <div id='review_cards_wrapper'>
+        {data?.map((review, index) => {
+          return (
+            <ReviewCard 
+              key={`reviewCard_${index}`}
+              review={review.review}
+              name={review.name}
+              role={review.role}
+              rating={review.rating}
+            />
+          )
+        })}
+      </div>
+    )
   return (
-    <div>Reviews</div>
+    <div>{content}</div>
   )
 }
