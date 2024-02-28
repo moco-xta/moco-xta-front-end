cd86db0c2419a325061edb0eb858a95360e0e2e7
moco-xta <moco.xta@gmail.com>
2024-02-28 10:51:32 +0100

    Projects: global structure

--- src/app/[locale]/projects/page.tsx
+++ src/app/[locale]/projects/page.tsx
@@ -1,5 +1,24 @@
 import React from 'react'
 
+import './index.scss'
+
 export default function Projects() {
-  return <div>Projects</div>
+  return (
+    <div id='timeline_container'>
+      <div id='school_and_companies_logos_container'>
+        <p>Openclassrooms</p>
+        <p>The Brink</p>
+        <p>Ponthus</p>
+      </div>
+      <div id='timeline'>
+        <p>Timeline</p>
+      </div>
+      <div id='projects_container'>
+        <p>Project 1</p>
+        <p>Project 2</p>
+        <p>Project 3</p>
+        <p>Project 4</p>
+      </div>
+    </div>
+  )
 }
