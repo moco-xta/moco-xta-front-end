4227b5727bac4bcea7be93b2fa4d39a3584b235d
moco-xta <moco.xta@gmail.com>
2024-02-28 01:51:51 +0100

    ReviewsSlice: get 3 first reviews ISSUE with css...  & Link to all reviews + Reviews page (rfc)

--- src/components/slices/reviews_slice/index.tsx
+++ src/components/slices/reviews_slice/index.tsx
@@ -1,17 +1,56 @@
+'use client'
+
 import React from 'react'
 import Link from 'next/link'
+
+import { useGetReviewsQuery } from '@/redux/api/reviewApi'
+
+import ReviewCard from '@/components/cards/review_card'
 
 import { Routes } from '@/routes/routes'
 
 import './index.scss'
 
 export default function ReviewsSlice() {
+  const { data, isLoading, error } = useGetReviewsQuery()
+
+  let content = null
+
+  if (isLoading) content = <div>Is loading...</div>
+
+  if (error) content = <div>An error occured...</div>
+
+  if (data)
+    content = (
+      <div id='review_cards_wrapper'>
+        {data?.map((review, index) => {
+          if(index < 3) {
+            return (
+              <ReviewCard 
+                key={`reviewCard_${index}`}
+                review={review.review}
+                name={review.name}
+                role={review.role}
+                rating={review.rating}
+              />
+            )
+          }
+        })}
+      </div>
+    )
+
   return (
     <div id='reviews_slice'>
-      <p>Reviews</p>
-      <Link href={Routes.find((route) => route.key === 'ADD_REVIEW')!.path}>
-        Add review
-      </Link>
+      <div id='reviews_container'>
+        <p>Reviews</p>
+        {content}
+        <Link href={Routes.find((route) => route.key === 'ADD_REVIEW')!.path}>
+          Add review
+        </Link>
+        <Link href={Routes.find((route) => route.key === 'REVIEWS')!.path}>
+          View all reviews
+        </Link>
+      </div>
     </div>
   )
 }
