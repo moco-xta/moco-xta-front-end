e06c00a3c04dc20da02ab7f67b6c2d18c15398f1
moco-xta <moco.xta@gmail.com>
2024-03-04 11:02:20 +0100

    HeadText: translation added

--- src/components/slices/hero_slice/head_text/index.tsx
+++ src/components/slices/hero_slice/head_text/index.tsx
@@ -1,24 +1,40 @@
 import React from 'react'
+import { useTranslations } from 'next-intl'
 import { MdComputer } from 'react-icons/md'
+
+import RevealComponent from '@/components/shared/reveal_component'
 
 import './index.scss'
 
 export default function HeadText() {
+  const t = useTranslations('HOME')
+
   return (
     <div id='head_text'>
       <p id='role'>
         <MdComputer />
         <span>Creative developer</span>
       </p>
-      <h1 id='heading'>
-        Hi, I'm <span className='gradient_text'>Federico</span>
-      </h1>
-      <h3 id='subheading'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text
-        ever since the 1500s, when an unknown printer took a galley of type
-        and scrambled it to make a type specimen book.
-      </h3>
+      <RevealComponent
+        x={100}
+        y={100}
+        duration={'500ms'}
+        threshold={0.75}
+      >
+        <h1 id='heading'>
+          {t('HERO.HI_I_AM')} <span className='gradient_text'>Federico</span>
+        </h1>
+      </RevealComponent>
+      <RevealComponent
+        x={100}
+        y={100}
+        duration={'500ms'}
+        threshold={0.75}
+      >
+        <h3 id='subheading'>
+          {t('HERO.SUBHEADING')}
+        </h3>
+      </RevealComponent>
     </div>
   )
 }
