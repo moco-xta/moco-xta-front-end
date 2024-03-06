a62d6de980c5c20c87fb95dd83bd49e1d0700df7
moco-xta <moco.xta@gmail.com>
2024-03-06 20:38:09 +0100

    ReviewCard: effect added on mouse mover hover

--- src/components/cards/review_card/index.tsx
+++ src/components/cards/review_card/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { RefObject, useEffect, useRef } from 'react'
 
 import { ReviewCardInterface } from '@/interfaces/components/cards/reviewCardInterface'
-import RatingStars from './rating_stars'
+
+import RatingStars from '@/components/cards/review_card/rating_stars'
+
+import { getUvMousePositionOnDiv } from '@/helpers/cssHelpers'
 
 import './index.scss'
 
@@ -11,8 +14,39 @@ export default function ReviewCard({
   role,
   rating,
 }: ReviewCardInterface) {
+  const cardRef = useRef<HTMLElement>(null)
+
+  function handleMouseMove(event: MouseEvent): void {
+    console.log('event', event)
+    const { x, y } = getUvMousePositionOnDiv(event)
+    cardRef.current!.style.transform = `perspective(1000px) rotateX(${y * 8}deg) rotateY(${x * 12}deg) scale3d(1.1, 1.1, 1.1)`
+    cardRef.current!.style.transition =
+    'all 100ms ease-in-out'
+  }
+
+  function handleMouseLeave(event: MouseEvent): void {
+    cardRef.current!.style.transform =
+    'perspective(1000px) rotateX(0deg) rotateY(0deg)scale3d(1, 1, 1)'
+    cardRef.current!.style.transition =
+    'all 250ms ease-in-out'
+  }
+
+  useEffect(() => {
+    cardRef.current!.addEventListener('mousemove', handleMouseMove)
+    cardRef.current!.addEventListener('mouseleave', handleMouseLeave)
+    return () => {
+      if (cardRef.current) {
+        cardRef.current.removeEventListener('mousemove', handleMouseMove)
+        cardRef.current.removeEventListener('mouseleave', handleMouseLeave)
+      }
+    }
+  }, [])
+
   return (
-    <div id='review_card'>
+    <div
+      ref={cardRef as RefObject<HTMLDivElement>}
+      id='review_card'
+    >
       <p className='quote_up'>"</p>
       <p className='review'>{review}</p>
       <p className='quote_down'>"</p>
