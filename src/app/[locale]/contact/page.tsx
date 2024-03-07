e520ac77f8b82fb7b82e1fd41a55d7e5bda50a49
moco-xta <moco.xta@gmail.com>
2024-03-07 12:26:08 +0100

    Contact: design OK -> picture

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -1,6 +1,12 @@
 'use client'
 
 import React from 'react'
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { CiMobile1 } from "react-icons/ci";
+import { MdOutlineEmail } from "react-icons/md";
 
 import RevealComponent from '@/components/shared/reveal_component'
 
@@ -8,33 +14,68 @@ import './index.scss'
 
 export default function Contact() {
   return (
-    <div id='contact_container'>
-      <div id='contact_head_text'>
-        <RevealComponent
-          x={100}
-          y={100}
-          duration={'500ms'}
-          threshold={0.75}
-        >
-          <p>Federico</p>
-          <p>Desmoulin</p>
-        </RevealComponent>
+    <div id='contact'>
+      <div id='contact_container'>
+        <div id='contact_head_text'>
+          <RevealComponent
+            y={100}
+            duration={'500ms'}
+            threshold={0.75}
+          >
+            <h1 className='gradient_text'>Federico Desmoulin</h1>
+            {/* <h2>Web developer</h2> */}
+          </RevealComponent>
+        </div>
+        <div id='why_socials_contacts_container'>
+          <RevealComponent
+            y={100}
+            duration={'500ms'}
+            threshold={0.75}
+          >
+            <div id='why_container'>
+              <h2>Why?</h2>
+              <p id='why'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            </div>
+            <div id='socials_container'>
+              <h2>Socials</h2>
+              <div className='social_container'>
+                <FaLinkedinIn className='social_icon' size={30} />
+                <span>Federico Desmoulin</span>
+              </div>
+              <div className='social_container'>
+                <FaTwitter className='social_icon' size={30} />
+                <span>@moco_xta</span>
+              </div>
+              <div className='social_container'>
+                <FaInstagram className='social_icon' size={30} />
+                <span>moco_xta</span>
+              </div>
+              <div className='social_container'>
+                <FaFacebookF className='social_icon' size={30} />
+                <span>Moco.xta</span>
+              </div>
+            </div>
+            <div id='contacts_container'>
+              <h2>Contact</h2>
+              <div className='contact_container'>
+                <CiMobile1 className='contact_icon' size={30} />
+                <p>+33 6 35 67 87 75</p>
+              </div>
+              <div className='contact_container'>
+                <MdOutlineEmail className='contact_icon' size={30} />
+                <p>moco.xta@gmail.com</p>
+              </div>
+            </div>
+          </RevealComponent>
+        </div>
       </div>
-      <div id='contact_socials_container'>
+      <div id='my_role'>
         <RevealComponent
-          x={100}
-          y={100}
+          y={50}
           duration={'500ms'}
           threshold={0.75}
         >
-          <div>
-            <h2>Social</h2>
-          </div>
-          <div>
-            <h2>Contact</h2>
-            <p>+33 6 35 67 87 75</p>
-            <p>moco.xta@gmail.com</p>
-          </div>
+          <p>Creative Developer</p>
         </RevealComponent>
       </div>
     </div>
