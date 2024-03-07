1af23546445bd602bc07fe867a8b48d556106b0b
moco-xta <moco.xta@gmail.com>
2024-03-07 09:51:38 +0100

    AddReview: base structure OK

--- src/components/cards/review_card/index.tsx
+++ src/components/cards/review_card/index.tsx
@@ -45,12 +45,12 @@ export default function ReviewCard({
       ref={cardRef as RefObject<HTMLDivElement>}
       id='review_card'
     >
-      <p className='quote_up'>"</p>
+      {review && <p className='quote_up'>"</p>}
       <p className='review'>{review}</p>
-      <p className='quote_down'>"</p>
-      <div className='rating_stars_container'>
+      {review && <p className='quote_down'>"</p>}
+      {rating !== 0 && <div className='rating_stars_container'>
         <RatingStars rating={rating} />
-      </div>
+      </div>}
       <div className='name_role_container'>
         <p className='name'>{name}</p>
         <p className='role'>{role}</p>
