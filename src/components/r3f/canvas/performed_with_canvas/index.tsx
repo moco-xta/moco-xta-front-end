544c8beb9c0f56d72ab3c466b5f970ded337b81f
moco-xta <moco.xta@gmail.com>
2024-03-08 12:14:51 +0100

    Clean

--- src/components/r3f/canvas/performed_with_canvas/index.tsx
+++ src/components/r3f/canvas/performed_with_canvas/index.tsx
@@ -1,32 +1,22 @@
 'use client'
 
-import React, { Ref, useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import * as THREE from 'three'
-import { Canvas, MeshProps, useFrame } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
 import { PerspectiveCamera } from '@react-three/drei'
 
 import { iconsDataInterface } from '@/interfaces/components/r3f/iconsDataInterface'
 
-import LogoRedux from '@/components/r3f/models/logos/LogoRedux'
-import LogoGithub from '@/components/r3f/models/logos/LogoGithub'
-import LogoFigma from '../../models/logos/LogoFigma'
-import LogoHtml from '../../models/logos/LogoHtml'
-import LogoNpm from '../../models/logos/LogoNpm'
 import LogoSass from '../../models/logos/LogoSass'
 
 function PerformedWithScene() {
-  const logoFigmaRef = useRef(new THREE.Object3D())
-  const logoGithubRef = useRef(new THREE.Object3D())
-  const logoHtmlRef = useRef(new THREE.Object3D())
-  const logoNpmRef = useRef(new THREE.Object3D())
-  const logoReduxRef = useRef(new THREE.Object3D())
   const logoSassRef = useRef<THREE.Mesh>(new THREE.Mesh())
 
   const refs = [
     logoSassRef,
   ]
 
-  const logoData: iconsDataInterface = {
+  const logosData: iconsDataInterface = {
     total_length: 0,
     widths: [],
   }
@@ -34,28 +24,28 @@ function PerformedWithScene() {
   useEffect(() => {
     refs.forEach((ref) => {
       // @ts-ignore
-      logoData.widths.push(ref.current.width)
+      logosData.widths.push(ref.current.width)
     })
     let sum = 0
     refs.forEach((ref, index) => {
       if (index === 0) {
         ref.current.position.x = sum
-        sum += logoData.widths[index] / 2
+        sum += logosData.widths[index] / 2
       } else {
-        ref.current.position.x = sum + logoData.widths[index] / 2 + index * 0.5
-        sum += logoData.widths[index]
+        ref.current.position.x = sum + logosData.widths[index] / 2 + index * 0.5
+        sum += logosData.widths[index]
       }
     })
-    logoData.widths.forEach((width) => {
-      logoData.total_length += width + 0.5
+    logosData.widths.forEach((width) => {
+      logosData.total_length += width + 0.5
     })
   }, [])
 
   useFrame((state, delta, xrFrame) => {
     refs.forEach((ref) => {
       ref.current.position.x += delta
-      if (ref.current.position.x > logoData.total_length / 2)
-        ref.current.position.x -= logoData.total_length
+      if (ref.current.position.x > logosData.total_length / 2)
+        ref.current.position.x -= logosData.total_length
     })
   })
 
@@ -74,18 +64,6 @@ function PerformedWithScene() {
         position={[-5, 5, 5]}
         castShadow
       />
-      {/* <LogoFigma
-        ref={logoFigmaRef}
-        scale={new THREE.Vector3(0.7, 0.7, 1)}
-      />
-      <LogoGithub ref={logoGithubRef} />
-      <LogoHtml ref={logoHtmlRef} />
-      <LogoNpm
-        ref={logoNpmRef}
-        scale={new THREE.Vector3(0.4, 0.4, 1)}
-      />
-      <LogoRedux ref={logoReduxRef} /> */}
-      {/* <LogoSass ref={logoSassRef} /> */}
       <LogoSass ref={logoSassRef} />
     </>
   )
