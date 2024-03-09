ec4f865de03b201d5800e2890ae2b7d6d7de34f8
moco-xta <moco.xta@gmail.com>
2024-03-09 12:16:19 +0100

    ProjectsSlice: refs created for each project + projectsData + ProjectsDataInterface

--- src/components/slices/projects_slice/index.tsx
+++ src/components/slices/projects_slice/index.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
+'use client'
+
+import React, { createRef, useEffect, useMemo } from 'react'
+
+import { projectsData } from '@/data/projetcsData'
 
 import './index.scss'
 
 export default function ProjectSlice() {
+
+  const projectsRefs = useMemo(
+    () =>
+      projectsData.map(() => ({
+        ref: createRef<HTMLParagraphElement>(),
+      })),
+    [],
+  )
+
+  useEffect(() => {
+    console.log(projectsRefs)
+  }, [])
+
   return (
     <div>
       <h1>Title</h1>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      {projectsRefs.map(({ ref }, index) => (
+        <p key={`project_description_${index}`} ref={ref}>{projectsData[index].description}</p>
+      ))}
     </div>
   )
 }
