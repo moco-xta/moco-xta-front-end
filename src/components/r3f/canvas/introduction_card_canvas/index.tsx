e03ca1b5b077a8d9811333384f7a35b371367e33
moco-xta <moco.xta@gmail.com>
2024-03-09 09:23:06 +0100

    IntroductionSlice: reorganized + IntroductionCardCanvas: index.tsx with content as props & index.scss

---
+++ src/components/r3f/canvas/introduction_card_canvas/index.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import React from 'react'
+
+import { IntroductionCardCanvasInterface } from '@/interfaces/components/r3f/introductionCardInterfaces'
+
+export default function IntroductionCardCanvas({ content }: IntroductionCardCanvasInterface) {
+  return (
+    <div>index</div>
+  )
+}
