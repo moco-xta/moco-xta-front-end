328d15efc71d49bd1c54c388de68190d76436af6
moco-xta <moco.xta@gmail.com>
2024-03-09 09:29:10 +0100

    IntroductionCardCanvas: Canvas & SoftShadows & Suspense + IntroductionCardScene: default Box

--- src/components/r3f/canvas/introduction_card_canvas/index.tsx
+++ src/components/r3f/canvas/introduction_card_canvas/index.tsx
@@ -1,11 +1,35 @@
 'use client'
 
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Box, SoftShadows } from '@react-three/drei'
 
-import { IntroductionCardCanvasInterface } from '@/interfaces/components/r3f/introductionCardInterfaces'
+import { IntroductionCardCanvasInterface, IntroductionCardSceneInterface } from '@/interfaces/components/r3f/introductionCardInterfaces'
+
+function IntroducitonCardScene({ content }: IntroductionCardSceneInterface) {
+  return (
+    <Box />
+  )
+}
 
 export default function IntroductionCardCanvas({ content }: IntroductionCardCanvasInterface) {
   return (
-    <div>index</div>
+    <Canvas
+      shadows
+      legacy
+      gl={{
+        antialias: true,
+        alpha: true,
+        powerPreference: 'high-performance',
+      }}
+    >
+      <SoftShadows
+        size={20}
+        samples={10}
+      />
+      <Suspense fallback={null}>
+        <IntroducitonCardScene content={content} />
+      </Suspense>
+    </Canvas>
   )
 }
