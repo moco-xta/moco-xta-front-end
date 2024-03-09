4c599211cf869cf50e93e6924cf42253e6937149
moco-xta <moco.xta@gmail.com>
2024-03-09 23:13:07 +0100

    ProjectsCanvas: free of Theatre.js

--- src/components/r3f/canvas/projects_canvas/index.tsx
+++ src/components/r3f/canvas/projects_canvas/index.tsx
@@ -2,7 +2,7 @@
 
 import React, { Suspense } from 'react'
 import * as THREE from 'three'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import { DepthOfField, EffectComposer } from '@react-three/postprocessing'
 import {
   Bloom,
@@ -25,30 +25,15 @@ import {
   KernelSize,
   Resolution,
 } from 'postprocessing'
-import studio from '@theatre/studio'
-import extension from '@theatre/r3f/dist/extension'
-import { ScrollControls, useScroll } from '@react-three/drei'
-import { getProject, val } from '@theatre/core'
-import { PerspectiveCamera, SheetProvider, useCurrentSheet } from '@theatre/r3f'
+import { PerspectiveCamera } from '@react-three/drei'
 
 import { ProjectsMap } from '@/components/r3f/models/projects/ProjectsMap'
 
-studio.extend(extension)
-studio.initialize()
-
 function ProjectsScene() {
-  const sheet = useCurrentSheet()
-  const scroll = useScroll()
-
-  useFrame(() => {
-    const sequenceLength = val(sheet!.sequence.pointer.length)
-    sheet!.sequence.position = scroll.offset * sequenceLength
-  })
 
   return (
     <>
       <PerspectiveCamera
-        theatreKey='Camera'
         makeDefault
         position={[0, 0, 0]}
         fov={90}
@@ -161,20 +146,11 @@ function ProjectsScene() {
 }
 
 export default function ProjectCanvas() {
-  const sheet = getProject('Skills scene').sheet('Scene')
 
   return (
     <Canvas gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={null}>
-        <ScrollControls
-          pages={5}
-          damping={1}
-          maxSpeed={1}
-        >
-          <SheetProvider sheet={sheet}>
-            <ProjectsScene />
-          </SheetProvider>
-        </ScrollControls>
+        <ProjectsScene />
       </Suspense>
     </Canvas>
   )
