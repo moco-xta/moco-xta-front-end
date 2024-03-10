68b2a7cb0612ee34a804b5723e6531a4cf72fbff
moco-xta <moco.xta@gmail.com>
2024-03-10 21:18:46 +0100

    ScrollToTop: logic done NEED design

--- src/components/buttons/scroll_to_top/index.tsx
+++ src/components/buttons/scroll_to_top/index.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useRef, useState } from 'react'
+import { FaArrowUp } from 'react-icons/fa'
 
 import './index.scss'
 
-export default function SrollToTop() {
+export default function ScrollToTop() {
+  const progressRef = useRef<HTMLDivElement>(null!)
+
+  function handleScroll() {
+    const position = window.scrollY
+    const height = document.documentElement.offsetHeight - document.documentElement.clientHeight
+    const scrollPercentage = (position * 100) / height
+    console.log('precentage', scrollPercentage)
+    if(position > 0) {
+      progressRef.current.style.display = 'grid'
+    } else {
+      progressRef.current.style.display = 'none'
+    }
+    /* progressRef.current.style.backgroundColor = `conic-gradient (#03cc65 ${scrollPercentage}%, #d7d7d7 ${scrollPercentage}%)`; */
+    progressRef.current.style.background = `conic-gradient(#03cc65 ${scrollPercentage}%, #d7d7d7 ${scrollPercentage}%)`
+    /* progressRef.current.style.backgroundColor = `rgba(${scrollPercentage}, ${scrollPercentage}, ${scrollPercentage}, 1)` */
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true })
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
   return (
-    <div>index</div>
+    <div
+      ref={progressRef}
+      id='progress'
+      onClick={scrollToTop}
+    >
+      <span
+        id='progress_value'
+      >
+        <FaArrowUp />
+      </span>
+    </div>
   )
 }
--- src/components/icons/scroll_button/index.tsx
+++ src/components/icons/scroll_button/index.tsx
@@ -3,7 +3,5 @@ import React from 'react'
 import './index.scss'
 
 export default function ScrollIcon() {
-  return (
-    <div id='scroll_icon' />
-  )
+  return <div id='scroll_icon' />
 }
