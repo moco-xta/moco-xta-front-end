a1ad8de8dbc6fc1880e9b2d08cbd212dfe0ca479
moco-xta <moco.xta@gmail.com>
2024-03-11 16:20:43 +0100

    HeroCanvas: OccupySpace gltf imported + lights

--- src/components/r3f/canvas/hero_canvas/index.tsx
+++ src/components/r3f/canvas/hero_canvas/index.tsx
@@ -1,9 +1,21 @@
 'use client'
 
-import React, { Suspense } from 'react'
+import React, { Suspense, useRef } from 'react'
 import * as THREE from 'three'
 import { Canvas } from '@react-three/fiber'
 import { PerspectiveCamera } from '@react-three/drei'
+import { OccupySpace } from '../../models/hero/OccupySpace'
+
+function HeroScene() {
+  const occupySpaceRef = useRef<THREE.Mesh>(null!)
+
+  return (
+    <OccupySpace
+      ref={occupySpaceRef}
+      scale={[2.5, 2.5, 2.5]}
+    />
+  )
+}
 
 export default function HeroCanvas() {
   return (
@@ -23,8 +35,24 @@ export default function HeroCanvas() {
           position={[0, 0, 15]}
           fov={30}
           near={0.1}
-          far={70}
+          far={55}
         />
+        <pointLight
+          position={[5, 5, 5]}
+          intensity={50}
+          castShadow
+        />
+        <pointLight
+          position={[-5, 5, 5]}
+          intensity={50}
+          castShadow
+        />
+        <pointLight
+          position={[-0, 5, 0]}
+          intensity={50}
+          castShadow
+        />
+        <HeroScene />
       </Suspense>
     </Canvas>
   )
