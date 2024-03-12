9469f855a6ee2a90c56d0a651a0c7226a3c51ece
moco-xta <moco.xta@gmail.com>
2024-03-12 12:36:23 +0100

    IntroductionCanvas: all variables exported in IntroductionConstants

--- src/data/introductionSkillsCardsData.ts
+++ src/data/introductionSkillsCardsData.ts
@@ -7,48 +7,77 @@ import { default as IntroductionConstants } from '@/constants/introductionConsta
 
 export const introductionSkillsCardsData: IntroductionCardDataInterface[] = [
   {
-    name: IntroductionConstants.DOUGHNUT_CHART.NAMES.THREE_D,
-    logo: DoughnutChartNinetyPercent,
-    descriptionKey: IntroductionConstants.DOUGHNUT_CHART.DESCRIPTION_KEY.THREE_D,
-    position: {
-      x: IntroductionConstants.DOUGHNUT_CHART.POSITION.X,
-      y: IntroductionConstants.DOUGHNUT_CHART.POSITION.Y,
-      z: IntroductionConstants.DOUGHNUT_CHART.POSITION.Z,
+    name: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.NAMES.THREE_D,
+    logo: {
+      component: DoughnutChartNinetyPercent,
+      position: {
+        x: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.POSITION.X,
+        y: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.POSITION.Y,
+        z: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.POSITION.Z,
+      },
+      scale: {
+        x: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.SCALE.X,
+        y: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.SCALE.Y,
+        z: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.SCALE.Z,
+      },
     },
-    scale: {
-      x: IntroductionConstants.DOUGHNUT_CHART.SCALE.X,
-      y: IntroductionConstants.DOUGHNUT_CHART.SCALE.Y,
-      z: IntroductionConstants.DOUGHNUT_CHART.SCALE.Z,
-    }
+    description: {
+      key: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.DESCRIPTION_KEYS.THREE_D,
+      position: {
+        x: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.TEXTS.POSITION.X,
+        y: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.TEXTS.POSITION.Y,
+        z: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.TEXTS.POSITION.Z,
+      },
+    },
   },
   {
-    name: IntroductionConstants.DOUGHNUT_CHART.NAMES.FRONT_END,
-    logo: DoughnutChartNinetyPercent,
-    descriptionKey: IntroductionConstants.DOUGHNUT_CHART.DESCRIPTION_KEY.FRONT_END,
-    position: {
-      x: IntroductionConstants.DOUGHNUT_CHART.POSITION.X,
-      y: IntroductionConstants.DOUGHNUT_CHART.POSITION.Y,
-      z: IntroductionConstants.DOUGHNUT_CHART.POSITION.Z,
+    name: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.NAMES.FRONT_END,
+    logo: {
+      component: DoughnutChartNinetyPercent,
+      position: {
+        x: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.POSITION.X,
+        y: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.POSITION.Y,
+        z: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.POSITION.Z,
+      },
+      scale: {
+        x: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.SCALE.X,
+        y: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.SCALE.Y,
+        z: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.SCALE.Z,
+      },
     },
-    scale: {
-      x: IntroductionConstants.DOUGHNUT_CHART.SCALE.X,
-      y: IntroductionConstants.DOUGHNUT_CHART.SCALE.Y,
-      z: IntroductionConstants.DOUGHNUT_CHART.SCALE.Z,
-    }
+    description: {
+      key: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.DESCRIPTION_KEYS
+        .FRONT_END,
+      position: {
+        x: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.TEXTS.POSITION.X,
+        y: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.TEXTS.POSITION.Y,
+        z: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.TEXTS.POSITION.Z,
+      },
+    },
   },
   {
-    name: IntroductionConstants.DOUGHNUT_CHART.NAMES.BACK_END,
-    logo: DoughnutChartSeventyFivePercent,
-    descriptionKey: IntroductionConstants.DOUGHNUT_CHART.DESCRIPTION_KEY.BACK_END,
-    position: {
-      x: IntroductionConstants.DOUGHNUT_CHART.POSITION.X,
-      y: IntroductionConstants.DOUGHNUT_CHART.POSITION.Y,
-      z: IntroductionConstants.DOUGHNUT_CHART.POSITION.Z,
+    name: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.NAMES.BACK_END,
+    logo: {
+      component: DoughnutChartNinetyPercent,
+      position: {
+        x: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.POSITION.X,
+        y: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.POSITION.Y,
+        z: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.POSITION.Z,
+      },
+      scale: {
+        x: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.SCALE.X,
+        y: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.SCALE.Y,
+        z: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.SCALE.Z,
+      },
     },
-    scale: {
-      x: IntroductionConstants.DOUGHNUT_CHART.SCALE.X,
-      y: IntroductionConstants.DOUGHNUT_CHART.SCALE.Y,
-      z: IntroductionConstants.DOUGHNUT_CHART.SCALE.Z,
-    }
+    description: {
+      key: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.DESCRIPTION_KEYS
+        .BACK_END,
+      position: {
+        x: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.TEXTS.POSITION.X,
+        y: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.TEXTS.POSITION.Y,
+        z: IntroductionConstants.CARDS.DOUGHNUT_CHARTS.TEXTS.POSITION.Z,
+      },
+    },
   },
 ]
--- src/interfaces/data/introductionCardDataInterface.ts
+++ src/interfaces/data/introductionCardDataInterface.ts
@@ -5,18 +5,25 @@ import { LogoRefType } from './performedWithDataInterface'
 
 export interface IntroductionCardDataInterface {
   name: string
-  logo:
-    | ForwardRefExoticComponent<
-        Omit<MeshProps, 'ref'> & RefAttributes<LogoRefType>
-      >
-    | ForwardRefExoticComponent<
-        Omit<MeshProps, 'ref'> & RefAttributes<THREE.Mesh>
-      >
-  descriptionKey: string
-  position: {
-    [key: string]: number
+  logo: {
+    component:
+      | ForwardRefExoticComponent<
+          Omit<MeshProps, 'ref'> & RefAttributes<LogoRefType>
+        >
+      | ForwardRefExoticComponent<
+          Omit<MeshProps, 'ref'> & RefAttributes<THREE.Mesh>
+        >
+    position: {
+      [key: string]: number
+    }
+    scale: {
+      [key: string]: number
+    }
   }
-  scale: {
-    [key: string]: number
+  description: {
+    key: string
+    position: {
+      [key: string]: number
+    }
   }
 }
