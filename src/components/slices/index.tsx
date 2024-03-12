f1954091bd9e39a2ab2f95c78858015c79e0112d
moco-xta <moco.xta@gmail.com>
2024-03-12 13:31:29 +0100

    LastProjectsSlice: index.tsx (rfc) & index.scss + Imported in home page

--- src/components/slices/index.tsx
+++ src/components/slices/index.tsx
@@ -1,6 +1,7 @@
 import HeroSlice from '@/components/slices/hero_slice'
 import IntroductionSlice from '@/components/slices/introduction_slice'
+import LastProjectsSlice from '@/components/slices/last_projects_slice'
 import PerformedWithSlice from '@/components/slices/performed_with_slice'
 import ReviewsSlice from '@/components/slices/reviews_slice'
 
-export { HeroSlice, IntroductionSlice, PerformedWithSlice, ReviewsSlice }
+export { HeroSlice, IntroductionSlice, LastProjectsSlice, PerformedWithSlice, ReviewsSlice }
