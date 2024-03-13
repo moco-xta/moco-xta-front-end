8a260a902eaa1d615b9e8011ca7da5a4856aa019
moco-xta <moco.xta@gmail.com>
2024-03-13 20:10:31 +0100

    SkillsCanvas: canvas configuration & imported in SkillsSlice + skillsConstants

--- src/components/r3f/canvas/skills_canvas/index.tsx
+++ src/components/r3f/canvas/skills_canvas/index.tsx
@@ -1,59 +1,27 @@
 'use client'
 
+import { Canvas } from '@react-three/fiber'
 import React, { Suspense } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import studio from '@theatre/studio'
-import extension from '@theatre/r3f/dist/extension'
-import { ScrollControls, useScroll } from '@react-three/drei'
-import { getProject, val } from '@theatre/core'
-import { PerspectiveCamera, SheetProvider, useCurrentSheet } from '@theatre/r3f'
+import { Box } from '@react-three/drei'
 
-import { SkillsPattern } from '@/components/r3f/models/skills/SkillsPattern'
+import { default as SkillsConstants } from '@/constants/skillsConstants.json'
 
-studio.extend(extension)
-studio.initialize()
-
-function SkillsScene() {
-  const sheet = useCurrentSheet()
-  const scroll = useScroll()
-
-  useFrame(() => {
-    const sequenceLength = val(sheet!.sequence.pointer.length)
-    sheet!.sequence.position = scroll.offset * sequenceLength
-  })
-
+export default function SkillCanvas() {
   return (
-    <>
-      <PerspectiveCamera
-        theatreKey='Camera'
-        makeDefault
-        position={[0, 0, 0]}
-        fov={90}
-        near={0.1}
-        far={70}
-      />
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <SkillsPattern />
-    </>
-  )
-}
-
-export default function SkillsCanvas() {
-  const sheet = getProject('Skills scene').sheet('Scene')
-
-  return (
-    <Canvas gl={{ preserveDrawingBuffer: true }}>
+    <Canvas
+      dpr={SkillsConstants.SCENE.CANVAS.DPR}
+      shadows
+      linear
+      flat
+      gl={{
+        antialias: SkillsConstants.SCENE.CANVAS.ANTIALIAS,
+        powerPreference: SkillsConstants.SCENE.CANVAS.POWER_PREFERENCE,
+        preserveDrawingBuffer:
+        SkillsConstants.SCENE.CANVAS.PRESERVE_DRAWING_BUFFER,
+      }}
+    >
       <Suspense fallback={null}>
-        <ScrollControls
-          pages={5}
-          damping={1}
-          maxSpeed={1}
-        >
-          <SheetProvider sheet={sheet}>
-            <SkillsScene />
-          </SheetProvider>
-        </ScrollControls>
+        <Box />
       </Suspense>
     </Canvas>
   )
---
+++ src/components/slices/skills_slice/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import SkillCanvas from '@/components/r3f/canvas/skills_canvas'
+
+import './index.scss'
+
+export default function SkillsSlice() {
+  return (
+    <div id='skill_slice'>
+      <SkillCanvas />
+    </div>
+  )
+}
