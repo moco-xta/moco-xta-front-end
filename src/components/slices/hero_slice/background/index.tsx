303503098d3699b07be76c4506ae9e9a8c2a962d
moco-xta <moco.xta@gmail.com>
2024-03-14 20:41:04 +0100

    HeroSclice: animation nackground circle

--- src/components/slices/hero_slice/background/index.tsx
+++ src/components/slices/hero_slice/background/index.tsx
@@ -1,22 +1,59 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
 
 import variables from '@/styles/variables.module.scss'
 import './index.scss'
 
 export default function Background() {
+  gsap.registerPlugin(useGSAP)
+  const timeline = gsap.timeline()
+
+  const container = useRef<SVGSVGElement>(null!)
+
+  useGSAP(
+    () => {
+      timeline
+        .fromTo(
+          '#circle',
+          {
+            r: 0,
+            opacity: 0
+          },
+          {
+            duration: 2,
+            ease: 'power3.out',
+            r: 450,
+            opacity: 1
+          }
+        )
+        /* .fromTo(
+          '#gradient_in_stop',
+          {
+            offset: '90%',
+          },
+          {
+            duration: 6,
+            ease: 'power3.out',
+            offset: '10%',
+          }
+        ) */
+    },
+    { scope: container },
+  )
+
   return (
     <div id='background_container'>
       <svg
+        ref={container}
         id='svg'
         viewBox='-500 -500 1000 1000'
         xmlns='http://www.w3.org/2000/svg'
-        /* xmlns="http://www.w3.org/2000/svg" */
-        /* xmlns:xlink="http://www.w3.org/1999/xlink" */
       >
         <defs>
           <radialGradient id='myGradient'>
             <stop
-              offset='10%'
+              id='gradient_in_stop'
               stop-color={variables.circle_gradient_in}
             />
             <stop
@@ -25,27 +62,11 @@ export default function Background() {
             />
           </radialGradient>
         </defs>
-        <filter id='displacementFilter'>
-          <feTurbulence
-            type='turbulence'
-            baseFrequency='4'
-            numOctaves='2'
-            result='turbulence'
-          />
-          <feDisplacementMap
-            in2='turbulence'
-            in='SourceGraphic'
-            scale='50'
-            xChannelSelector='R'
-            yChannelSelector='G'
-          />
-        </filter>
         <filter id='blurMe'>
           <feGaussianBlur stdDeviation='5' />
         </filter>
         <circle
           id='circle'
-          r='450'
           fill="url('#myGradient')"
           filter='url(#blurMe)'
         />
