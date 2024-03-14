c0a2738f3567ba496b647d725eff559e3c198a09
moco-xta <moco.xta@gmail.com>
2024-03-14 01:38:46 +0100

    useProjectsTimeline()

--- src/components/slices/projects_slice/index.tsx
+++ src/components/slices/projects_slice/index.tsx
@@ -1,16 +1,14 @@
 'use client'
 
-import React, { createRef, useEffect, useMemo, useState } from 'react'
+import React, { createRef, useEffect, useMemo } from 'react'
 
 import { projectsData } from '@/data/projectsData'
 import { companiesAndSchollData } from '@/data/companiesAndSchoolData'
 
-import useScroll from '@/hooks/useScroll'
+import useProjectsTimeline from '@/hooks/useProjectsTimeline'
 
-import { getDifferenceBetweenTwoDatesInDays, isValidDate } from '@/helpers/dateHelpers'
+import { isValidDate } from '@/helpers/dateHelpers'
 
-/* import ProjectsContent from './projects_content' */
-/* import ProjectCanvas from '@/components/r3f/canvas/projects_canvas' */
 import CurrentDate from './current_date'
 import { LastProjectsCard } from '@/components/cards/last_projects_card'
 import { CompanyOrSchoolCard } from '@/components/cards/company_or_school_card'
@@ -18,19 +16,13 @@ import { CompanyOrSchoolCard } from '@/components/cards/company_or_school_card'
 import './index.scss'
 
 export default function ProjectsSlice() {
-  const { y, offsetHeight, clientHeight, scrollFlow } = useScroll()
-
-  const [currentProject, setCurrentProject] = useState<number>(0)
-  const [currentCompanyOrSchool, setCurrentCompanyOrSchool] =
-    useState<number>(0)
-  const [currentDate, setCurrentDate] = useState<Date>(new Date())
-  const [daysDifference] = useState<number>(
-    getDifferenceBetweenTwoDatesInDays(
-      new Date(),
-      new Date(projectsData[projectsData.length - 1].dates.from),
-    ),
-  )
-  const [deltaPerDay, setDeltaPerDay] = useState<number>(0)
+  const {
+    scrollFlow,
+    currentProject,
+    currentCompanyOrSchool,
+    currentDate,
+    dataLoaded,
+  } = useProjectsTimeline(projectsData, companiesAndSchollData)
 
   const projectsRefs = useMemo(
     () =>
@@ -48,47 +40,13 @@ export default function ProjectsSlice() {
   )
 
   useEffect(() => {
-    if (offsetHeight && clientHeight) {
-      setCurrentDate(new Date())
-      setDeltaPerDay((offsetHeight - clientHeight) / daysDifference)
-
-      projectsRefs.forEach(({ ref }, index) => {
-        if (ref.current) ref.current.style.top = `${(index + 1) * 400}px`
-      })
-      companiesAndSchoolRefs.forEach(({ ref }, index) => {
-        if (ref.current) ref.current.style.top = `${(index + 1) * 400}px`
-      })
-    }
-  }, [offsetHeight, clientHeight])
-
-  useEffect(() => {
-    const differenceBetweenTodayAndCurrentDate = Math.round(y / deltaPerDay)
-    setCurrentDate(
-      new Date(
-        new Date().setDate(
-          new Date().getDate() - differenceBetweenTodayAndCurrentDate,
-        ),
-      ),
-    )
-  }, [y])
-
-  useEffect(() => {
-    projectsData.forEach((project, index) => {
-      if (
-        currentDate.getTime() > new Date(project.dates.from).getTime() &&
-        currentDate.getTime() < new Date(project.dates.to).getTime()
-      )
-        setCurrentProject(index)
+    projectsRefs.forEach(({ ref }, index) => {
+      if (ref.current) ref.current.style.top = `${(index + 1) * 400}px`
     })
-    companiesAndSchollData.forEach((companyOrSchool, index) => {
-      if (
-        currentDate.getTime() >
-          new Date(companyOrSchool.dates.from).getTime() &&
-        currentDate.getTime() < new Date(companyOrSchool.dates.to).getTime()
-      )
-        setCurrentCompanyOrSchool(index)
+    companiesAndSchoolRefs.forEach(({ ref }, index) => {
+      if (ref.current) ref.current.style.top = `${(index + 1) * 400}px`
     })
-  }, [currentDate])
+  }, [dataLoaded])
 
   useEffect(() => {
     projectsRefs.forEach(({ ref }) => {
