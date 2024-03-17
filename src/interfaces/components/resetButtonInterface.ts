0d71e633c51e1230fa195ef14f0f15ffde38e9a8
moco-xta <moco.xta@gmail.com>
2024-03-17 17:05:11 +0100

    SubmitButton + ResetButton

---
+++ src/interfaces/components/resetButtonInterface.ts
@@ -0,0 +1,7 @@
+import { FormikState } from "formik";
+
+import { AddReviewValuesInterface } from "../addReviewValuesInterface";
+
+export interface ResetButtonInterface {
+  resetForm: (nextState?: Partial<FormikState<AddReviewValuesInterface>> | undefined) => void
+}
