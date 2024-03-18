afe6e64b3297ef71a5d4d506eb7a6c2ef30a9a0f
moco-xta <moco.xta@gmail.com>
2024-03-18 10:41:21 +0100

    Skills: basic structure

---
+++ src/components/slices/skills_slice/front_end_skills/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useTranslations } from 'next-intl'
+
+import ThreeDSkillsCanvas from '@/components/r3f/canvas/three_d_skills_canvas'
+
+import './index.scss'
+
+export default function FrontEndSkills() {
+  const t = useTranslations('SKILLS')
+
+  return (
+    <div className='skills_block_container'>
+      <div className='skills_block_text'>
+        <h3>Front-end</h3>
+        <p className='skills_block_text_paragraph'>{t('PARAGRAPHS.FRONT_END')}</p>
+      </div>
+      <div className='skills_separator'/>
+      <div className='skills_canvas_container'>
+        <ThreeDSkillsCanvas />
+      </div>
+    </div>
+  )
+}
--- src/components/slices/skills_slice/index.tsx
+++ src/components/slices/skills_slice/index.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import { useTranslations } from 'next-intl'
 
-import SkillCanvas from '@/components/r3f/canvas/skills_canvas'
+import ThreeDSkills from './three_d_skills'
+import FrontEndSkills from './front_end_skills'
+import MobileSkills from './mobile_skills'
+import BackEndSkills from './back_end_skills'
+import DatabaseSkills from './database_skills'
+import ToolsSkills from './tools_skills'
 
 import './index.scss'
 
 export default function SkillsSlice() {
+  const t = useTranslations('SKILLS')
+
   return (
-    <div id='skill_slice'>
-      <SkillCanvas />
-    </div>
+    <section id='skills_slice'>
+      <div id='skills_content'>
+        <h2>{t('SKILLS')}</h2>
+        <ThreeDSkills />
+        <FrontEndSkills />
+        <MobileSkills />
+        <BackEndSkills />
+        <DatabaseSkills />
+        <ToolsSkills />
+      </div>
+    </section>
   )
 }
