c17a0b33b36496eaf314061a43a47a42d45049db
moco-xta <moco.xta@gmail.com>
2024-03-18 10:41:21 +0100

    Skills: basic structure

---
+++ src/components/r3f/canvas/three_d_skills_canvas/index.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { Canvas } from '@react-three/fiber'
+import React, { Suspense } from 'react'
+import { Box } from '@react-three/drei'
+
+import { default as SkillsConstants } from '@/constants/skillsConstants.json'
+
+export default function ThreeDSkillsCanvas() {
+  return (
+    <Canvas
+      dpr={SkillsConstants.SCENE.CANVAS.DPR}
+      shadows
+      linear
+      flat
+      gl={{
+        antialias: SkillsConstants.SCENE.CANVAS.ANTIALIAS,
+        powerPreference: SkillsConstants.SCENE.CANVAS.POWER_PREFERENCE,
+        preserveDrawingBuffer:
+          SkillsConstants.SCENE.CANVAS.PRESERVE_DRAWING_BUFFER,
+      }}
+    >
+      <Suspense fallback={null}>
+        <Box />
+      </Suspense>
+    </Canvas>
+  )
+}
