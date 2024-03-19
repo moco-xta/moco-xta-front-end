0629687b5c79d24fa90adc0e5d87c4c3d64ca6a5
moco-xta <moco.xta@gmail.com>
2024-03-19 12:38:10 +0100

    SeeAllProjectlink: InternalLink added

--- src/components/slices/last_projects_slice/see_all_projects_link/index.tsx
+++ src/components/slices/last_projects_slice/see_all_projects_link/index.tsx
@@ -1,19 +1,13 @@
 import React from 'react'
-import Link from 'next/link'
-import { FaArrowRight } from 'react-icons/fa6'
-import { useTranslations } from 'next-intl'
-
-import { Routes } from '@/routes/routes'
 
 import RevealTextBlock from '@/components/shared/effects/reveal_text_block'
+import InternalLink from '@/components/links/internal_link'
 
 import { default as effectsConstants } from '@/constants/effectsConstants.json'
 
 import './index.scss'
 
 export default function SeeAllProjectsLink() {
-  const t = useTranslations('HOME')
-
   return (
     <div id='see_all_projects_link_container'>
       <RevealTextBlock
@@ -21,15 +15,10 @@ export default function SeeAllProjectsLink() {
         duration={effectsConstants.REVEAL_TEXT_BLOCK.DEFAULT.DURATION}
         y={effectsConstants.REVEAL_TEXT_BLOCK.DEFAULT.Y}
       >
-        <Link
-          id='see_all_projects_link'
-          href={Routes.find((route) => route.key === 'PROJECTS')!.path}
-        >
-          <span id='see_all_projects_link_text'>
-            {t('LAST_PROJECTS.SEE_ALL_PROJECTS')}
-          </span>
-          <FaArrowRight />
-        </Link>
+        <InternalLink
+          routeKey={'PROJECTS'}
+          translationKey={'HOME.LAST_PROJECTS.SEE_ALL_PROJECTS'}
+        />
       </RevealTextBlock>
     </div>
   )
