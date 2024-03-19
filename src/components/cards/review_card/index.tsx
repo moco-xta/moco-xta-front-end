2dd036a3749631133f748753c822ac7633374d9e
moco-xta <moco.xta@gmail.com>
2024-03-19 19:59:57 +0100

    LastReviewsCard: useCardHoverEffect() added

--- src/components/cards/review_card/index.tsx
+++ src/components/cards/review_card/index.tsx
@@ -1,15 +1,14 @@
 'use client'
 
-import React, { RefObject, useEffect, useRef } from 'react'
+import React, { useRef } from 'react'
 import { useTranslations } from 'next-intl'
 
 import { ReviewCardInterface } from '@/interfaces/reviewCardInterface'
 
 import RatingStars from '@/components/cards/review_card/rating_stars'
 
-import { getUvMousePositionOnDiv } from '@/helpers/cssHelpers'
-
 import './index.scss'
+import useCardHoverEffect from '@/hooks/useCardHoverEffect'
 
 export default function ReviewCard({
   review,
@@ -19,34 +18,13 @@ export default function ReviewCard({
 }: ReviewCardInterface) {
   const t = useTranslations('ADD_REVIEW')
 
-  const cardRef = useRef<HTMLElement>(null)
-
-  function handleMouseMove(event: MouseEvent): void {
-    const { x, y } = getUvMousePositionOnDiv(event)
-    cardRef.current!.style.transform = `perspective(1000px) rotateX(${y * 8}deg) rotateY(${x * 12}deg) scale3d(1.1, 1.1, 1.1)`
-    cardRef.current!.style.transition = 'all 100ms ease-in-out'
-  }
-
-  function handleMouseLeave(event: MouseEvent): void {
-    cardRef.current!.style.transform =
-      'perspective(1000px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)'
-    cardRef.current!.style.transition = 'all 100ms ease-in-out'
-  }
-
-  useEffect(() => {
-    cardRef.current!.addEventListener('mousemove', handleMouseMove)
-    cardRef.current!.addEventListener('mouseleave', handleMouseLeave)
-    return () => {
-      if (cardRef.current) {
-        cardRef.current.removeEventListener('mousemove', handleMouseMove)
-        cardRef.current.removeEventListener('mouseleave', handleMouseLeave)
-      }
-    }
-  }, [])
+  const cardRef = useRef<HTMLDivElement>(null)
+  
+  useCardHoverEffect(cardRef)
 
   return (
     <div
-      ref={cardRef as RefObject<HTMLDivElement>}
+      ref={cardRef}
       id='review_card'
     >
       {review && <p className='quote_up'>"</p>}
