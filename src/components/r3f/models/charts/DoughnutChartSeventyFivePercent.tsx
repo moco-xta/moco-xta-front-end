79ad02b447de4403d51893f97131abb0d29e13fd
moco-xta <moco.xta@gmail.com>
2024-03-19 12:26:45 +0100

    IntroductionCardCanvas: forwardRef removed + work in charts color BUT not conclusive yet...

--- src/components/r3f/models/charts/DoughnutChartSeventyFivePercent.tsx
+++ src/components/r3f/models/charts/DoughnutChartSeventyFivePercent.tsx
@@ -5,43 +5,43 @@ import { GLTF } from 'three-stdlib'
 
 import { default as GltfConstants } from '@/constants/gltfConstants.json'
 
-type GltfResultType = GLTF & {
+type GLTFResult = GLTF & {
   nodes: {
     DoughnutChartSeventyFivePercent: THREE.Mesh
   }
   materials: {}
 }
 
-export const DoughnutChartSeventyFivePercent = forwardRef<
-  THREE.Mesh,
-  JSX.IntrinsicElements['mesh']
->(({ position, scale }, ref) => {
-  const gltf = useGLTF(
+export function DoughnutChartSeventyFivePercent(
+  props: JSX.IntrinsicElements['group'],
+) {
+  const { nodes } = useGLTF(
     GltfConstants.DOUGHNUT_CHART_SEVENTY_FIVE_PERCENT,
-  ) as GltfResultType
-
+  ) as GLTFResult
   return (
-    <mesh
-      ref={ref}
-      geometry={gltf.nodes.DoughnutChartSeventyFivePercent.geometry}
-      position={position}
-      scale={scale}
-      receiveShadow
-      castShadow
+    <group
+      {...props}
+      dispose={null}
     >
-      <meshPhysicalMaterial
-        color={'hotpink'}
-        specularColor={'hotpink'}
-        roughness={1}
-        metalness={0}
-        iridescence={1}
-        clearcoat={0}
-        reflectivity={0}
-        emissive={'hotpink'}
-        emissiveIntensity={1}
-      />
-    </mesh>
+      <mesh
+        geometry={nodes.DoughnutChartSeventyFivePercent.geometry}
+        receiveShadow
+        castShadow
+      >
+        <meshPhysicalMaterial
+          color={new THREE.Color('rgb(256, 120, 80)')}
+          specularColor={new THREE.Color('rgb(256, 120, 80)')}
+          roughness={1}
+          metalness={0}
+          iridescence={1}
+          clearcoat={0}
+          reflectivity={0}
+          emissive={new THREE.Color('rgb(221, 0, 45)')}
+          emissiveIntensity={3.7}
+        />
+      </mesh>
+    </group>
   )
-})
+}
 
 useGLTF.preload(GltfConstants.DOUGHNUT_CHART_SEVENTY_FIVE_PERCENT)
