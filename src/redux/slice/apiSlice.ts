87ec43c655cc16452ee67fcb83b3070ba7ac66ba
moco-xta <moco.xta@gmail.com>
2024-03-21 17:10:20 +0100

    apiSlice: test baseUrl

--- src/redux/slice/apiSlice.ts
+++ src/redux/slice/apiSlice.ts
@@ -4,7 +4,8 @@ const apiSlice = createApi({
   reducerPath: 'api',
   tagTypes: ['Review', 'Test'],
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_BACK_END,
+    /* baseUrl: process.env.NEXT_PUBLIC_BACK_END, */
+    baseUrl: 'https://127.0.0.1:8080/test/v1/',
   }),
   endpoints: () => ({}),
 })
