25df7916e600f8e8c0be0828c96c6330c7406dd6
moco-xta <moco.xta@gmail.com>
2024-03-22 18:30:33 +0100

    next.config: output: 'export' removed

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,6 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin()
 
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
   webpack: (config, options) => {
     config.module.rules.push({
