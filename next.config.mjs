5704458d505d061692c38472f02b11ecaf0e3c20
moco-xta <moco.xta@gmail.com>
2024-03-22 17:59:33 +0100

    Docker: new configuration

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,6 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin()
 
 const nextConfig = {
-  output: 'standalone',
   reactStrictMode: true,
   webpack: (config, options) => {
     config.module.rules.push({
