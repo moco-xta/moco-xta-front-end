e0be83f31dd0c07eabbf02dadc8a76e5846051db
moco-xta <moco.xta@gmail.com>
2024-03-25 09:30:04 +0100

    authentication: Autentication (rfc) + SignUp (rfc) + SignIn (rfc)

---
+++ src/components/shared/authentication/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './index.scss'
+
+export default function Authentication() {
+  return (
+    <div>Authentication</div>
+  )
+}
---
+++ src/components/shared/authentication/sign_up/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './index.scss'
+
+export default function SignUp() {
+  return (
+    <div>Sign up</div>
+  )
+}
