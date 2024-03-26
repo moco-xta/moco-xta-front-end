b962140d9c091ba25aefcfdaeb5b34abe13f7bcf
moco-xta <moco.xta@gmail.com>
2024-03-26 09:59:26 +0100

    SignIn: signInValidationSchema

--- src/validations/signInValidationSchema.ts
+++ src/validations/signInValidationSchema.ts
@@ -1,11 +1,8 @@
 import * as Yup from 'yup'
 
 export const signInValidationSchema = Yup.object().shape({
-  firstName: Yup.string().required('FORMS.VALIDATIONS.REQUIRED'),
-  lastName: Yup.string().required('FORMS.VALIDATIONS.REQUIRED'),
   email: Yup.string()
     .email('FORMS.VALIDATIONS.MUST_BE_A_VALID_EMAIL')
     .required('FORMS.VALIDATIONS.REQUIRED'),
-  password: Yup.string().required('FORMS.VALIDATIONS.REQUIRED'),
-  confirmPassword: Yup.string().required('FORMS.VALIDATIONS.REQUIRED'),
+  password: Yup.string().required('FORMS.VALIDATIONS.REQUIRED')
 })
