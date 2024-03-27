38180038d77d58bbcd4a34d3ea0c70fa1fa22459
moco-xta <moco.xta@gmail.com>
2024-03-27 07:58:05 +0100

    HeroSlice: redesign head text

--- src/components/slices/hero_slice/head_text/index.tsx
+++ src/components/slices/hero_slice/head_text/index.tsx
@@ -12,29 +12,34 @@ export default function HeadText() {
   const t = useTranslations('HOME')
 
   return (
-    <div id='hero_head_text_container'>
-      <RevealTextBlock
-        threshold={effectsConstants.REVEAL_TEXT_BLOCK.DEFAULT.THRESHOLD}
-        duration={effectsConstants.REVEAL_TEXT_BLOCK.DEFAULT.DURATION}
-        y={effectsConstants.REVEAL_TEXT_BLOCK.DEFAULT.Y}
-      >
-        <h1 id='heading'>
-          {/* <RevealTextLetterByLetter text={`${t('HERO.HI_I_AM')} Federico`} /> */}
-          {t('HERO.HI_I_AM')}
-          <span> Federico</span>
-        </h1>
-      </RevealTextBlock>
-      <RevealTextBlock
-        threshold={effectsConstants.REVEAL_TEXT_BLOCK.DEFAULT.THRESHOLD}
-        duration={effectsConstants.REVEAL_TEXT_BLOCK.DEFAULT.DURATION}
-        y={effectsConstants.REVEAL_TEXT_BLOCK.DEFAULT.Y}
-        delay={
-          effectsConstants.REVEAL_TEXT_BLOCK.HOME.HERO.HEAD_TEXT.SUBHEADING
-            .DELAY
-        }
-      >
-        <h3 id='subheading'>{t('HERO.SUBHEADING')}</h3>
-      </RevealTextBlock>
+    <div id='hero_head_text'>
+      <div id='hero_head_text_container'>
+        <RevealTextBlock
+          threshold={effectsConstants.REVEAL_TEXT_BLOCK.DEFAULT.THRESHOLD}
+          duration={effectsConstants.REVEAL_TEXT_BLOCK.DEFAULT.DURATION}
+          y={effectsConstants.REVEAL_TEXT_BLOCK.DEFAULT.Y}
+        >
+          <h2 id='heading'>
+            {/* <RevealTextLetterByLetter text={`${t('HERO.HI_I_AM')} Federico`} /> */}
+            {t('HERO.HI_I_AM')}
+            <span> Federico</span>
+          </h2>
+        </RevealTextBlock>
+        <RevealTextBlock
+          threshold={effectsConstants.REVEAL_TEXT_BLOCK.DEFAULT.THRESHOLD}
+          duration={effectsConstants.REVEAL_TEXT_BLOCK.DEFAULT.DURATION}
+          y={effectsConstants.REVEAL_TEXT_BLOCK.DEFAULT.Y}
+          delay={
+            effectsConstants.REVEAL_TEXT_BLOCK.HOME.HERO.HEAD_TEXT.SUBHEADING
+              .DELAY
+          }
+        >
+          <h3 id='subheading'>{t('HERO.SUBHEADING')}</h3>
+        </RevealTextBlock>
+      </div>
+      <div id='creative_developer_container'>
+        <p>Creative Developer</p>
+      </div>
     </div>
   )
 }
--- src/components/slices/hero_slice/index.tsx
+++ src/components/slices/hero_slice/index.tsx
@@ -3,9 +3,7 @@
 import React from 'react'
 
 import HeadText from './head_text'
-import Background from './background'
 import HeroCanvas from '@/components/r3f/canvas/hero_canvas'
-import Role from './role'
 import ScrollDown from '@/components/shared/scroll_down'
 
 import './index.scss'
@@ -14,11 +12,10 @@ export default function HeroSlice() {
   return (
     <section id='hero_slice'>
       <HeadText />
-      <Background />
       <div id='hero_canvas_container'>
         <HeroCanvas />
       </div>
-      <Role />
+      
       <ScrollDown />
     </section>
   )
