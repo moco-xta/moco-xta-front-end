22659dfc0b88f4d74d71f53519eacff5ee1ea1a9
moco-xta <moco.xta@gmail.com>
2024-03-30 21:33:06 +0100

    Header: test logo

--- src/components/shared/header/logo/index.tsx
+++ src/components/shared/header/logo/index.tsx
@@ -4,12 +4,17 @@ import Link from 'next/link'
 import { Routes } from '@/routes/routes'
 
 import './index.scss'
+import { Canvas } from '@react-three/fiber'
+import { MocoLogo } from '@/components/r3f/models/moco_logo/MocoLogo'
+import { OrthographicCamera, PerspectiveCamera } from '@react-three/drei'
+
+import './index.scss'
 
 export default function Logo() {
   return (
     <div id='logo'>
       <Link href={Routes.find((route) => route.key === 'HOME')!.path}>
-        Logo
+        <img src="/img/png/moco_logo_light_h70.png" alt="Girl in a jacket" />
       </Link>
     </div>
   )
