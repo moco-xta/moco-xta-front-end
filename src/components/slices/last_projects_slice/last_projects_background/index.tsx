19789dc803f346516025f4ac8b61cfcaed48cb39
moco-xta <moco.xta@gmail.com>
2024-03-31 11:30:44 +0200

    LastProjectsBackground: animation GOOD

--- src/components/slices/last_projects_slice/last_projects_background/index.tsx
+++ src/components/slices/last_projects_slice/last_projects_background/index.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import useScroll from '@/hooks/useScroll'
+import React, { useEffect, useRef } from 'react'
 
 export default function LastProjectsBackground() {
+  const { y } = useScroll()
+
+  const backgroundRef = useRef<HTMLImageElement>(null!)
+
+  function setFrame(y: number) {
+    const frame = Math.round(((y + 1) / 15) + 1)
+    const frameToString = frame.toString()
+    const frameLength = frameToString.length
+    var finalFrame = ''
+    for(var i = 0; i < 4 - frameLength; i++) {
+      finalFrame += '0'
+    }
+    finalFrame += frameToString
+    return finalFrame
+  }
+
+  useEffect(() => {
+    backgroundRef.current.src = `/img/png/projects_animation/inflated_project_frame_${setFrame(y)}.png`
+  }, [backgroundRef, y])
+
   return (
-    <div>index</div>
+    <img
+      ref={backgroundRef}
+      id='last_projects_background'
+      src='/img/png/projects_animation/inflated_project_frame_0001.png'
+    />
   )
 }
