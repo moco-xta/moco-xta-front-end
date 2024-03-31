fb94ae6172f88d1f1a6d71cae7a5a53f7d2b1792
moco-xta <moco.xta@gmail.com>
2024-03-31 10:53:14 +0200

    LastProjectsSlice: background position

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
   return (
     <>
       <div id='home_container'>
-        <HeroSlice />
-        <IntroductionSlice />
+        {/* <HeroSlice />
+        <IntroductionSlice /> */}
         <LastProjectsSlice />
         <LastReviewsSlice />
         <PerformedWithSlice />
--- src/components/slices/last_projects_slice/index.tsx
+++ src/components/slices/last_projects_slice/index.tsx
@@ -5,12 +5,16 @@ import React from 'react'
 import LastProjectsText from './last_projects_text'
 import LastProjectsCards from './last_projects_cards'
 import SeeAllProjectsLink from './see_all_projects_link'
+import LastProjectsBackground from './last_projects_background'
 
 import './index.scss'
 
 export default function LastProjectsSlice() {
   return (
     <section id='last_projects_slice'>
+      <div id='last_projects_background_container'>
+        <LastProjectsBackground />
+      </div>
       <div id='last_projects_content'>
         <LastProjectsText />
         <LastProjectsCards />
