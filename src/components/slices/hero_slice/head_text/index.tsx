cd3465c0bc7cc5c971b3481cc68944ab519f5048
moco-xta <moco.xta@gmail.com>
2024-04-23 18:17:32 +0200

    Hero: HeadText for mobile

--- src/components/slices/hero_slice/head_text/index.tsx
+++ src/components/slices/hero_slice/head_text/index.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import { useTranslations } from 'next-intl'
 
-/* import RevealTextLetterByLetter from '@/components/shared/effects/reveal_text_letter_by_letter' */
-import RevealTextBlock from '@/components/shared/effects/reveal_text_block'
-
-import { default as effectsConstants } from '@/constants/effectsConstants.json'
-
 import './index.scss'
 
 export default function HeadText() {
