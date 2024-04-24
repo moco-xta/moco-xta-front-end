be5111720042fb34e3e60477b0f8d56773f72469
moco-xta <moco.xta@gmail.com>
2024-04-24 09:52:17 +0200

    HeroCanvas: issue fix with the C... in mobile

--- src/components/slices/hero_slice/index.tsx
+++ src/components/slices/hero_slice/index.tsx
@@ -7,7 +7,7 @@ import HeadText from './head_text'
 import HeroCanvas from '@/components/r3f/canvas/hero_canvas/HeroCanvas'
 import ScrollDown from '@/components/shared/scroll_down'
 
-import { default as imgConstants } from '@/constants/imgConstants.json'
+/* import { default as imgConstants } from '@/constants/imgConstants.json' */
 
 import variables from '@/styles/variables.module.scss'
 import './index.scss'
@@ -19,18 +19,16 @@ export default function HeroSlice() {
       <div id='hero_canvas_container'>
         <HeroCanvas />
       </div>
-      <>
-        <Image
-          src={imgConstants.HERO.ARIES.SRC}
-          width={imgConstants.HERO.ARIES.DIMENSIONS.WITDH}
-          height={imgConstants.HERO.ARIES.DIMENSIONS.HEIGHT}
-          alt='Aries'
-        />
-        <ScrollDown
-          top={'85vh'}
-          color={variables.white}
-        />
-      </>
+      {/* <Image
+        src={imgConstants.HERO.ARIES.SRC}
+        width={imgConstants.HERO.ARIES.DIMENSIONS.WITDH}
+        height={imgConstants.HERO.ARIES.DIMENSIONS.HEIGHT}
+        alt='Aries'
+      /> */}
+      <ScrollDown
+        top={'85vh'}
+        color={variables.white}
+      />
     </section>
   )
 }
