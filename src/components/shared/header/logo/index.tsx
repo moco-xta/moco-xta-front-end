1c067bf997526ab214c7d22f7254826b354c834a
moco-xta <moco.xta@gmail.com>
2024-05-04 12:00:57 +0200

    Header: logo ids renamed + clean

--- src/components/shared/header/logo/index.tsx
+++ src/components/shared/header/logo/index.tsx
@@ -1,33 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 
 import { Routes } from '@/routes/routes'
-
-import useIsDesktop from '@/hooks/useIsDesktop'
 
 import logoPic from '/public/img/png/moco_logo_light_h70.png'
 
 import './index.scss'
 
 export default function Logo() {
-  const { isDesktop } = useIsDesktop()
-
-  const [logoDimesion, setlogoDimension] = useState({
-    width: 109,
-    height: 70,
-  })
-
-  useEffect(() => {
-    isDesktop ? setlogoDimension({ width: 110, height: 70 }) : setlogoDimension({ width: 55, height: 35 })
-  }, [isDesktop])
-
   return (
-    <div id='logo'>
+    <div id='logo_container'>
       <Link href={Routes.find((route) => route.key === 'HOME')!.path}>
         <Image
           src={logoPic}
-          id='logo_test'
+          id='logo'
           alt='Logo header'
         />
       </Link>
