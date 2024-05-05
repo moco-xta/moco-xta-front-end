9aa487e9999e3914021b58054efd8a26c8070060
moco-xta <moco.xta@gmail.com>
2024-05-05 10:18:43 +0200

    Cleffy: commented

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -15,8 +15,8 @@ export default function Home() {
   return (
     <>
       <div id='home_container'>
-        {/* <HeroSlice /> */}
-        <CleffySlice />
+        <HeroSlice />
+        {/* <CleffySlice /> */}
         <IntroductionSlice />
         <LastProjectsSlice />
         <LastReviewsSlice />
