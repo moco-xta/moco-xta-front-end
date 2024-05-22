a1c56c33905a85c5e33f82471103bed9a590bc8b
moco-xta <moco.xta@gmail.com>
2024-05-22 09:59:35 +0200

    SignUpForm: done with design

--- src/components/new/shared/authentication/sign_up/index.tsx
+++ src/components/new/shared/authentication/sign_up/index.tsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { Formik } from 'formik'
+import { toast } from 'sonner'
+import { useTranslations } from 'next-intl'
 
 import { SignUpSignInInterface } from '@/interfaces/new/componentsInterfaces'
+import { SignUpValuesInterface } from '@/interfaces/new/reduxApiInterfaces'
+
+import { AppDispatch } from '@/redux/store'
+import { useSignUpMutation } from '@/redux/api/authenticationApi'
+import { setIsAuthenticated } from '@/redux/slice/authenticationSlice'
+
+import { signUpValidationSchema } from 'validations/signUpValidationSchema'
+
+import SignUpForm from './sign_up_form'
+
+import { clearFormStoredValues } from '@/helpers/localStorageHelpers'
 
 import './index.scss'
 
@@ -8,5 +23,63 @@ export default function SignUp({
   setIsSignIn,
   handleSetAuthenticationIsOpen,
 }: SignUpSignInInterface) {
-  return <div>Sign UP</div>
+  const t = useTranslations()
+
+  const dispatch = useDispatch<AppDispatch>()
+
+  const [signUp] = useSignUpMutation()
+
+  const [submitButtonIsDisabled, setSubmitButtonIsDisabled] =
+    useState<boolean>(false)
+  const [resetButtonIsDisabled, setResetButtonIsDisabled] =
+    useState<boolean>(true)
+
+  const initialValues: SignUpValuesInterface = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  }
+
+  return (
+    <div id='sign_in_container'>
+      <h1 className='form_title'>Sign up</h1>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={signUpValidationSchema}
+        onSubmit={(values, { resetForm }) => {
+          setSubmitButtonIsDisabled(true)
+          const payload = {
+            firstName: values.firstName,
+            lastName: values.lastName,
+            email: values.email,
+            password: values.password,
+            role: 'User',
+          }
+          toast.promise(signUp(payload).unwrap(), {
+            loading: t('TOASTERS.AUTHENTIFICATION.SIGN_IN.LOADING'),
+            success: () => {
+              dispatch(setIsAuthenticated(true))
+              resetForm({ values: initialValues })
+              clearFormStoredValues(initialValues)
+              setSubmitButtonIsDisabled(false)
+              handleSetAuthenticationIsOpen()
+              return t('TOASTERS.AUTHENTIFICATION.SIGN_IN.SUCCESS')
+            },
+            error: (response) => {
+              setSubmitButtonIsDisabled(false)
+              return response.data.message
+            },
+          })
+        }}
+      >
+        <SignUpForm
+          submitButtonIsDisabled={submitButtonIsDisabled}
+          resetButtonIsDisabled={resetButtonIsDisabled}
+          setIsSignIn={setIsSignIn}
+        />
+      </Formik>
+    </div>
+  )
 }
--- src/interfaces/new/reduxApiInterfaces.ts
+++ src/interfaces/new/reduxApiInterfaces.ts
@@ -3,6 +3,17 @@ export interface SignInPayloadInterface {
   password: string
 }
 
+export interface SignUpPayloadInterface {
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+}
+
+export interface SignUpValuesInterface extends SignUpPayloadInterface {
+  confirmPassword: string
+}
+
 export interface AddReviewValuesInterface {
   review: string
   role: 'project_manager' | 'developer' | 'designer' | 'other' | ''
