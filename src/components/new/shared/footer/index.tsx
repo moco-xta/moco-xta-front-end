7818360c0e4991cc55a874a1d4ab3d52b11bcb80
moco-xta <moco.xta@gmail.com>
2024-05-27 22:20:03 +0200

    Footer: socials + copyrights

--- src/components/new/shared/footer/index.tsx
+++ src/components/new/shared/footer/index.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { FaInstagram } from 'react-icons/fa'
+import { FaTwitter } from 'react-icons/fa'
+import { FaLinkedinIn } from 'react-icons/fa'
+import { IoLogoGithub } from 'react-icons/io'
 
 import './index.scss'
 
+const SIZE = 50
+
 export default function Footer() {
-  return <footer>FOOOOOOTER</footer>
+  return (
+    <footer>
+      <div id='footer_socials'>
+        <IoLogoGithub size={SIZE} />
+        <FaTwitter size={SIZE} />
+        <FaInstagram size={SIZE} />
+        <FaLinkedinIn size={SIZE} />
+      </div>
+      <p id='copyright'>
+        © Copyright 2024 Federico Desmoulin - All rights reserved
+      </p>
+    </footer>
+  )
 }
