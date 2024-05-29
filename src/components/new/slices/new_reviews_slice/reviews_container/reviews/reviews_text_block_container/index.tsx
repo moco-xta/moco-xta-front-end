ed071b970d2358617950c74c1cc52ac64ac9a75d
moco-xta <moco.xta@gmail.com>
2024-05-29 13:28:48 +0200

    reviews: ..centered_text_block applied to ReviewsTextBlock

--- src/components/new/slices/new_reviews_slice/reviews_container/reviews/reviews_text_block_container/index.tsx
+++ src/components/new/slices/new_reviews_slice/reviews_container/reviews/reviews_text_block_container/index.tsx
@@ -6,7 +6,6 @@ import useResize from '@/hooks/new/useResize'
 import AddReviewButton from '@/components/new/buttons/add_review_button'
 import TextBlockSimple from '@/components/new/shared/text_blocks/text_block_simple'
 
-import variables from '@/styles/new/variables.module.scss'
 import './index.scss'
 
 export default function ReviewsTextBlock() {
@@ -21,17 +20,7 @@ export default function ReviewsTextBlock() {
         id='reviews_text_block'
         title={'HOME.REVIEWS.TEXT_BLOCK.TITLE'}
         paragraph={'HOME.REVIEWS.TEXT_BLOCK.PARAGRAPH'}
-        blockStyle={
-          isDesktop
-            ? {
-                padding: `${variables.top_bottom_margin_medium} 35% 0 35%`,
-              }
-            : {
-                padding: `${variables.top_bottom_margin_small} ${variables.left_right_margin_smaller} 0 ${variables.left_right_margin_smaller}`,
-                display: 'flex',
-                flexDirection: 'column',
-              }
-        }
+        className={'centered_text_block'}
       />
     </div>
   )
