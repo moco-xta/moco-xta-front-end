357d003152c5121aedfc9772274d72bfbaaebe3e
moco-xta <moco.xta@gmail.com>
2024-06-03 10:01:20 +0200

    SnakeGame: colors updated snake cells + ambientLight intensity 1.8

--- src/components/new/three/canvas/snake_game_canvas/Lights.tsx
+++ src/components/new/three/canvas/snake_game_canvas/Lights.tsx
@@ -7,7 +7,7 @@ const INTENSITY = 300
 export default function Lights() {
   return (
     <>
-      <ambientLight intensity={1} />
+      <ambientLight intensity={1.8} />
       <pointLight
         position={[0, 20, 0]}
         intensity={INTENSITY}
