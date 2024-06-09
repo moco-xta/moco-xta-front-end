a734e50ea4bf95396760be7ce5288ccc2cca077b
moco-xta <moco.xta@gmail.com>
2024-06-09 19:23:18 +0200

    Footer: LogoGitHub + LogoLinkedIn

--- src/components/new/three/canvas/socials_canvas/Lights.tsx
+++ src/components/new/three/canvas/socials_canvas/Lights.tsx
@@ -2,8 +2,8 @@ import React, { useRef } from 'react'
 import * as THREE from 'three'
 import { useHelper } from '@react-three/drei'
 
-const INTENSITY = 1
-const MAP_SIZE = [2048, 2048]
+const INTENSITY = 1.25
+const MAP_SIZE = [4096, 4096]
 const SHADOW_CAMERA_FAR = 500
 const SHADOW_CAMERA_LEFT = -10
 const SHADOW_CAMERA_RIGHT = 10
@@ -13,8 +13,12 @@ const SHADOW_RADIUS = 3
 const SHADOW_BIAS = 0.0001
 
 export default function Lights() {
-  const directionalLightSWRef = useRef<THREE.DirectionalLight>(null!)
-  useHelper(directionalLightSWRef, THREE.DirectionalLightHelper, 1, 'red')
+  const directionalLightTLRef = useRef<THREE.DirectionalLight>(null!)
+  const directionalLightTRRef = useRef<THREE.DirectionalLight>(null!)
+  const directionalLightFRef = useRef<THREE.DirectionalLight>(null!)
+  useHelper(directionalLightTLRef, THREE.DirectionalLightHelper, 1, 'red')
+  useHelper(directionalLightTRRef, THREE.DirectionalLightHelper, 1, 'red')
+  useHelper(directionalLightFRef, THREE.DirectionalLightHelper, 1, 'red')
 
   const directionalLightTarget = new THREE.Vector3(
     0,
@@ -24,24 +28,9 @@ export default function Lights() {
 
   return (
     <>
-      <ambientLight intensity={1} />
+      <ambientLight intensity={1.5} />
       <directionalLight
-        ref={directionalLightSWRef}
-        position={[5, 5, 5]}
-        intensity={INTENSITY}
-        target-position={directionalLightTarget}
-        castShadow
-        shadow-mapSize={MAP_SIZE}
-        shadow-camera-far={SHADOW_CAMERA_FAR}
-        shadow-camera-left={SHADOW_CAMERA_LEFT}
-        shadow-camera-right={SHADOW_CAMERA_RIGHT}
-        shadow-camera-top={SHADOW_CAMERA_TOP}
-        shadow-camera-bottom={SHADOW_CAMERA_BOTTOM}
-        shadow-radius={SHADOW_RADIUS}
-        shadow-bias={SHADOW_BIAS}
-      />
-      <directionalLight
-        ref={directionalLightSWRef}
+        ref={directionalLightTLRef}
         position={[-5, 5, 5]}
         intensity={INTENSITY}
         target-position={directionalLightTarget}
@@ -56,7 +45,22 @@ export default function Lights() {
         shadow-bias={SHADOW_BIAS}
       />
       <directionalLight
-        ref={directionalLightSWRef}
+        ref={directionalLightTRRef}
+        position={[5, 5, 5]}
+        intensity={INTENSITY}
+        target-position={directionalLightTarget}
+        castShadow
+        shadow-mapSize={MAP_SIZE}
+        shadow-camera-far={SHADOW_CAMERA_FAR}
+        shadow-camera-left={SHADOW_CAMERA_LEFT}
+        shadow-camera-right={SHADOW_CAMERA_RIGHT}
+        shadow-camera-top={SHADOW_CAMERA_TOP}
+        shadow-camera-bottom={SHADOW_CAMERA_BOTTOM}
+        shadow-radius={SHADOW_RADIUS}
+        shadow-bias={SHADOW_BIAS}
+      />
+      <directionalLight
+        ref={directionalLightFRef}
         position={[0, 0, 5]}
         intensity={INTENSITY}
         target-position={directionalLightTarget}
--- src/components/new/three/canvas/socials_canvas/SocialsScene.tsx
+++ src/components/new/three/canvas/socials_canvas/SocialsScene.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+
 import { LogoTwitter } from '../../models/logos/LogoTwitter'
+import { LogoLinkedIn } from '../../models/logos/LogoLinkedIn'
+import { LogoGitHub } from '../../models/logos/LogoGitHub'
 
 export default function SocialsScene() {
   return (
-    <LogoTwitter />
+    <>
+      <LogoTwitter />
+      <LogoLinkedIn position={[2.5, 0, 0]} />
+      <LogoGitHub position={[-2.5, 0, 0]} />
+    </>
   )
 }
