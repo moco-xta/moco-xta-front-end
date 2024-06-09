05eece12fb2e7f79171bf773af7273449ba32c84
moco-xta <moco.xta@gmail.com>
2024-06-09 19:50:11 +0200

    About: test isMobile and permissionGranted

--- src/components/new/slices/about_slice/index.tsx
+++ src/components/new/slices/about_slice/index.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { isMobile } from 'react-device-detect'
 
 import './index.scss'
 
 export default function AboutSlice() {
-  return <section id='about_slice'>AboutSlice</section>
+  const [permissionGranted, setPermissionGranted] = useState(false)
+
+  return <section id='about_slice'>
+  {!isMobile || (isMobile && permissionGranted) ? (
+    <p>!isMobile || (isMobile && permissionGranted)</p>
+  ) : (
+    <p>isMobile && !permissionGranted</p>
+  )}
+  </section>
 }
