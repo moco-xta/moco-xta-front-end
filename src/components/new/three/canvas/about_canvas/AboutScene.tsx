3152b737410a3a319835fa2d93e219b3ae2e5407
moco-xta <moco.xta@gmail.com>
2024-06-09 21:54:02 +0200

    About: Player added -> test between Player and device

--- src/components/new/three/canvas/about_canvas/AboutScene.tsx
+++ src/components/new/three/canvas/about_canvas/AboutScene.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
-import { Box } from '@react-three/drei'
+import * as THREE from 'three'
+import { Box, Plane } from '@react-three/drei'
+import { RigidBody } from '@react-three/rapier'
 
 export default function AboutScene() {
   return (
-    <Box />
+    <>
+      <Box />
+      <RigidBody colliders='hull'>
+        <Plane
+          args={[50, 50, 20, 20]}
+          rotation={[THREE.MathUtils.degToRad(90), 0, 0]}
+        />
+      </RigidBody>
+    </>
   )
 }
