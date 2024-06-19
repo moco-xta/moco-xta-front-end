fd04216fc4f2a0c0a34ce2bf9a4a6bf5a1eec757
moco-xta <moco.xta@gmail.com>
2024-06-20 00:22:31 +0200

    About: Trestles + Lighting improved

--- src/components/new/three/canvas/about_canvas/LaboratoireFinalRenderLights.tsx
+++ src/components/new/three/canvas/about_canvas/LaboratoireFinalRenderLights.tsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react'
 import * as THREE from 'three'
 import { useHelper } from '@react-three/drei'
 
-const INTENSITY = 1.8
+const INTENSITY = 1
 const MAP_SIZE = [4096, 4096]
 const SHADOW_CAMERA_FAR = 500
 const SHADOW_CAMERA_LEFT = -500
@@ -13,24 +13,43 @@ const SHADOW_RADIUS = 3
 const SHADOW_BIAS = 0.0001
 
 export default function LaboratoireFinalRenderLights() {
-  const directionalLightRef = useRef<THREE.DirectionalLight>(null!)
-  useHelper(directionalLightRef, THREE.DirectionalLightHelper, 1, 'red')
+  const directionalLightCeilingLeftRef = useRef<THREE.DirectionalLight>(null!)
+  const directionalLightCeilingRightRef = useRef<THREE.DirectionalLight>(null!)
+  useHelper(directionalLightCeilingLeftRef, THREE.DirectionalLightHelper, 1, 'blue')
+  useHelper(directionalLightCeilingRightRef, THREE.DirectionalLightHelper, 1, 'red')
 
   return (
-    <directionalLight
-      ref={directionalLightRef}
-      position={[0, 20, 0]}
-      intensity={INTENSITY}
-      target-position={new THREE.Vector3(0, 0, 0)}
-      castShadow
-      shadow-mapSize={MAP_SIZE}
-      shadow-camera-far={SHADOW_CAMERA_FAR}
-      shadow-camera-left={SHADOW_CAMERA_LEFT}
-      shadow-camera-right={SHADOW_CAMERA_RIGHT}
-      shadow-camera-top={SHADOW_CAMERA_TOP}
-      shadow-camera-bottom={SHADOW_CAMERA_BOTTOM}
-      shadow-radius={SHADOW_RADIUS}
-      shadow-bias={SHADOW_BIAS}
-    />
+    <>
+      <directionalLight
+        ref={directionalLightCeilingLeftRef}
+        position={[-5, 28.6, 0]}
+        intensity={INTENSITY}
+        target-position={new THREE.Vector3(-5, 0, 0)}
+        castShadow
+        shadow-mapSize={MAP_SIZE}
+        shadow-camera-far={SHADOW_CAMERA_FAR}
+        shadow-camera-left={SHADOW_CAMERA_LEFT}
+        shadow-camera-right={SHADOW_CAMERA_RIGHT}
+        shadow-camera-top={SHADOW_CAMERA_TOP}
+        shadow-camera-bottom={SHADOW_CAMERA_BOTTOM}
+        shadow-radius={SHADOW_RADIUS}
+        shadow-bias={SHADOW_BIAS}
+      />
+      <directionalLight
+        ref={directionalLightCeilingRightRef}
+        position={[5, 28.6, 0]}
+        intensity={INTENSITY}
+        target-position={new THREE.Vector3(5, 0, 0)}
+        castShadow
+        shadow-mapSize={MAP_SIZE}
+        shadow-camera-far={SHADOW_CAMERA_FAR}
+        shadow-camera-left={SHADOW_CAMERA_LEFT}
+        shadow-camera-right={SHADOW_CAMERA_RIGHT}
+        shadow-camera-top={SHADOW_CAMERA_TOP}
+        shadow-camera-bottom={SHADOW_CAMERA_BOTTOM}
+        shadow-radius={SHADOW_RADIUS}
+        shadow-bias={SHADOW_BIAS}
+      />
+    </>
   )
 }
--- src/components/new/three/models/about/Trestle.tsx
+++ src/components/new/three/models/about/Trestle.tsx
@@ -1,7 +1,39 @@
+import * as THREE from 'three'
 import React from 'react'
+import { useGLTF } from '@react-three/drei'
+import { GLTF } from 'three-stdlib'
+
+import useTextureLoader from '@/hooks/useTextureLoader'
 
 import { default as gltfConstants } from '@/constants/new/assets/gltfConstants.json'
+import { default as texturesConstants } from '@/constants/new/assets/texturesConstants.json'
 
-export default function Trestle() {
-  return <div>Trestle</div>
+type GLTFResult = GLTF & {
+  nodes: {
+    Trestle: THREE.Mesh
+  }
+  materials: {}
 }
+
+export function Trestle({ position }: JSX.IntrinsicElements['group']) {
+  const { nodes } = useGLTF(gltfConstants.ABOUT.TRESTLE) as GLTFResult
+
+  const map = useTextureLoader(texturesConstants.ABOUT.TRESTLE.DIFFUSE.FOUR_K)
+  map.flipY = false
+
+  return (
+    <mesh
+      geometry={nodes.Trestle.geometry}
+      position={position}
+      receiveShadow
+      castShadow
+    >
+      <meshStandardMaterial
+        attach='material'
+        map={map}
+      />
+    </mesh>
+  )
+}
+
+useGLTF.preload(gltfConstants.ABOUT.TRESTLE)
