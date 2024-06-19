20d920d2e65753c2b8523d62f6f08925215fe6ce
moco-xta <moco.xta@gmail.com>
2024-06-19 10:49:33 +0200

    About: Instructions translations DONE

--- src/components/new/buttons/about_enter_button/index.tsx
+++ src/components/new/buttons/about_enter_button/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useTranslations } from 'next-intl'
 
 import { AboutEnterButtonInterface } from '@/interfaces/new/buttonsInterfaces'
 
 import './index.scss'
 
 export default function AboutEnterButton({  }: AboutEnterButtonInterface) {
+  const t = useTranslations('BUTTONS')
+
   return (
-    <button id='about_enter_button'>Enter</button>
+    <button id='about_enter_button'>{t('ENTER')}</button>
   )
 }
--- src/components/new/slices/about_slice/about_greeting/intructions/desktop_instructions/index.tsx
+++ src/components/new/slices/about_slice/about_greeting/intructions/desktop_instructions/index.tsx
@@ -7,15 +7,18 @@ import {
   FaMousePointer,
 } from 'react-icons/fa'
 import { LuTouchpad } from 'react-icons/lu'
+import { useTranslations } from 'next-intl'
 
 import AboutEnterButton from '@/components/new/buttons/about_enter_button'
 
 import './index.scss'
 
 export default function DesktopInstructions() {
+  const t = useTranslations('ABOUT')
+
   return (
     <div id='desktop_instructions'>
-      <h2 id='desktop_instructions_title'>Instructions</h2>
+      <h2 id='desktop_instructions_title'>{t('INSTRUCTIONS')}</h2>
       <div id='displacement_desktop_instructions'>
         <div id='displacement_keyboard_keys'>
           <FaArrowUp className='keyboard_key' />
@@ -25,20 +28,20 @@ export default function DesktopInstructions() {
             <FaArrowRight className='keyboard_key' />
           </div>
         </div>
-        <p>Utilisez les fleches pour vous deplacer</p>
+        <p>{t('USE_THE_KEYBOARD_ARROWS_TO_MOVE')}</p>
       </div>
       <p id='camera_orientation_desktop_instructions'>
-        Use{' '}
+        {t('USE')}{' '}
         <FaMousePointer
           id='mouse_pointer_icon'
           size={40}
         />{' '}
-        or{' '}
+        {t('OR')}{' '}
         <LuTouchpad
           id='touchpad_icon'
           size={40}
         />{' '}
-        pour orienter la camera
+        {t('TO_DIRECT_THE_CAMERA')}
       </p>
       <AboutEnterButton />
     </div>
