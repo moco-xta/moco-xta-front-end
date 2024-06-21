b9bea71ed46d57005ace99896feb39d2d26ed764
moco-xta <moco.xta@gmail.com>
2024-06-21 11:50:00 +0200

    AboutCanvas: [FINAL RENDER] AmorLlamaAmor -> HoySi

--- src/components/new/three/canvas/about_canvas/Frames.tsx
+++ src/components/new/three/canvas/about_canvas/Frames.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import { AmorLlamaAmor } from '../../models/about/AmorLlamaAmor'
 import { Trinity } from '../../models/about/Trinity'
 
 export default function Frames() {
   return (
     <>
+      <AmorLlamaAmor position={[-11.2, 10, -16]} />
       <Trinity position={[-17.09, 19.2, -16]} />
     </>
   )
---
+++ src/components/new/three/models/about/AmorLlamaAmor.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import * as THREE from 'three'
+import { useGLTF } from '@react-three/drei'
+import { GLTF } from 'three-stdlib'
+
+import useTextureLoader from '@/hooks/useTextureLoader'
+
+import { default as gltfConstants } from '@/constants/new/assets/gltfConstants.json'
+import { default as texturesConstants } from '@/constants/new/assets/texturesConstants.json'
+
+type GLTFResult = GLTF & {
+  nodes: {
+    AmorLlamaAmor_1: THREE.Mesh
+    AmorLlamaAmor_2: THREE.Mesh
+  }
+  materials: {}
+}
+
+export function AmorLlamaAmor({ position }: JSX.IntrinsicElements['mesh']) {
+  const { nodes } = useGLTF(gltfConstants.ABOUT.AMOR_LLAMA_AMOR) as GLTFResult
+
+  const amorllamaAmorMap = useTextureLoader(
+    texturesConstants.ABOUT.FINAL.HOY_SI.DIFFUSE.ONE_K,
+  )
+  amorllamaAmorMap.flipY = false
+
+  const amorLlamaAmorMaterial = new THREE.MeshStandardMaterial({
+    /* color: '#808080', */
+    map: amorllamaAmorMap,
+    roughness: 1
+  })
+
+  const amorLlamaAmorFrameMaterial = new THREE.MeshStandardMaterial({
+    color: '#000000',
+    roughness: 0.6
+  })
+
+  return (
+    <group
+      name='AmorLlamaAmor'
+      position={position}
+      dispose={null}
+    >
+      <mesh
+        name='AmorLlamaAmor_1'
+        geometry={nodes.AmorLlamaAmor_1.geometry}
+        material={amorLlamaAmorFrameMaterial}
+        receiveShadow
+        castShadow
+      />
+      <mesh
+        name='AmorLlamaAmor_2'
+        geometry={nodes.AmorLlamaAmor_2.geometry}
+        material={amorLlamaAmorMaterial}
+        receiveShadow
+        castShadow
+      />
+    </group>
+  )
+}
+
+useGLTF.preload(gltfConstants.ABOUT.AMOR_LLAMA_AMOR)
