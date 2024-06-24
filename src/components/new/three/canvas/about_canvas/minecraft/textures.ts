e75dde69d2bebedd43924e2c2b1ba141a3c7a94b
moco-xta <moco.xta@gmail.com>
2024-06-24 11:53:30 +0200

    AboutCanvas: [MINECRAFT] minecraft textures loader

---
+++ src/components/new/three/canvas/about_canvas/minecraft/textures.ts
@@ -0,0 +1,9 @@
+import useTextureLoader from '@/hooks/useTextureLoader'
+
+import { default as texturesConstants } from '@/constants/new/assets/texturesConstants.json'
+
+export const dirtTexture = useTextureLoader(texturesConstants.MINECRAFT.DIRT)
+export const glassTexture = useTextureLoader(texturesConstants.MINECRAFT.GLASS)
+export const grassTexture = useTextureLoader(texturesConstants.MINECRAFT.GRASS)
+export const logTexture = useTextureLoader(texturesConstants.MINECRAFT.LOG)
+export const woodTexture = useTextureLoader(texturesConstants.MINECRAFT.WOOD)
