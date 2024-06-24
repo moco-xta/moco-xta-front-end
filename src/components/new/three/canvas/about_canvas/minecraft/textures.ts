c691d1b3096c6c911b03425b5afd2c04bf70e83c
moco-xta <moco.xta@gmail.com>
2024-06-24 11:55:52 +0200

    npm i @react-three/cannon --save

--- src/components/new/three/canvas/about_canvas/minecraft/textures.ts
+++ src/components/new/three/canvas/about_canvas/minecraft/textures.ts
@@ -5,5 +5,6 @@ import { default as texturesConstants } from '@/constants/new/assets/texturesCon
 export const dirtTexture = useTextureLoader(texturesConstants.MINECRAFT.DIRT)
 export const glassTexture = useTextureLoader(texturesConstants.MINECRAFT.GLASS)
 export const grassTexture = useTextureLoader(texturesConstants.MINECRAFT.GRASS)
+export const groundTexture = useTextureLoader(texturesConstants.MINECRAFT.GRASS)
 export const logTexture = useTextureLoader(texturesConstants.MINECRAFT.LOG)
 export const woodTexture = useTextureLoader(texturesConstants.MINECRAFT.WOOD)
