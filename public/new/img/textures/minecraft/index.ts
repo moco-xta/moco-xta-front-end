b860ae3c5d272ca893fc906a75e5003514d835e5
moco-xta <moco.xta@gmail.com>
2024-06-24 11:34:48 +0200

    AboutCanvas: [MINECRAFT] minecraft textures

---
+++ public/new/img/textures/minecraft/index.ts
@@ -0,0 +1,13 @@
+import dirtTexture from './dirt.jpg'
+import glassTexture from './glass.png'
+import grassTexture from './grass.jpg'
+import logTexture from './log.jpg'
+import woodTexture from './wood.jpg'
+
+export {
+  dirtTexture,
+  glassTexture,
+  grassTexture,
+  logTexture,
+  woodTexture
+}
