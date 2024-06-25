e0a4c6acdd382f3df60375017818a175eb547c30
moco-xta <moco.xta@gmail.com>
2024-06-25 09:13:11 +0200

    About: [STRAT REWRITE] aboutSlice rfc

--- src/components/new/slices/about_slice/index.tsx
+++ src/components/new/slices/about_slice/index.tsx
@@ -1,53 +1,9 @@
-import React, { useState } from 'react'
-import { isMobile } from 'react-device-detect'
-
-import AboutGreeting from './about_greeting'
-import QRCodeToAbout from './qr_code_to_about'
-import AboutDashboard from './about_dahboard'
-import AboutCanvas from '../../three/canvas/about_canvas/AboutCanvas'
-import DeviceMotionPermission from './device_motion_permission'
+import React from 'react'
 
 import './index.scss'
-import AboutTarget from './about_dahboard/about_target'
-import { TextureSelector } from '../../three/canvas/about_canvas/minecraft/TextureSelector'
 
 export default function AboutSlice() {
-  const [permissionGranted, setPermissionGranted] = useState<boolean>(false)
-  const [showInstructions, setShowInstructions] = useState<boolean>(true)
-  const [isClayRender, setIsClayRender] = useState<boolean>(true)
-
-  const handleSetIsClayRender = () => {
-    setIsClayRender(!isClayRender)
-  }
-
   return (
-    <section id='about_slice'>
-      {!isMobile || (isMobile && permissionGranted) ? (
-        <>
-          <AboutGreeting
-            showInstructions={showInstructions}
-            handleSetIsClayRender={handleSetIsClayRender}
-          />
-          <AboutTarget />
-          {!isMobile && (
-            <>
-              {/* <QRCodeToAbout /> */}
-              {/* <AboutDashboard showInstructions={showInstructions} /> */}
-            </>
-          )}
-          <AboutCanvas
-            showInstructions={showInstructions}
-            setShowInstructions={setShowInstructions}
-            isClayRender={isClayRender}
-          />
-          <TextureSelector />
-        </>
-      ) : (
-        <DeviceMotionPermission
-          permissionGranted={permissionGranted}
-          setPermissionGranted={setPermissionGranted}
-        />
-      )}
-    </section>
+    <div id='about_slice'>index</div>
   )
 }
