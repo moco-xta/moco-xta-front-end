1768fa5c5de1093c1bc8b630a4b9155af38203c5
moco-xta <moco.xta@gmail.com>
2024-06-25 23:34:55 +0200

    npx prettier --write .

--- src/components/new/three/models/about/laboratoire/final_render/MacBookPro.tsx
+++ src/components/new/three/models/about/laboratoire/final_render/MacBookPro.tsx
@@ -21,7 +21,9 @@ type GLTFResult = GLTF & {
 }
 
 export function MacBookPro({ position }: JSX.IntrinsicElements['group']) {
-  const { nodes } = useGLTF(gltfConstants.ABOUT.LABORATOIRE.FINAL_RENDER.MAC_BOOK_PRO) as GLTFResult
+  const { nodes } = useGLTF(
+    gltfConstants.ABOUT.LABORATOIRE.FINAL_RENDER.MAC_BOOK_PRO,
+  ) as GLTFResult
 
   return (
     <group
