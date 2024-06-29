8aee854052ab42c06bd0199362b9438d24d912f9
moco-xta <moco.xta@gmail.com>
2024-06-29 20:06:22 +0200

    AboutCanvas: [MOBILE VERSION] test to get window.DeviceMotionEvent data

--- src/components/new/slices/about_slice/about_dahboard/about_target/index.tsx
+++ src/components/new/slices/about_slice/about_dahboard/about_target/index.tsx
@@ -3,5 +3,11 @@ import React from 'react'
 import './index.scss'
 
 export default function AboutTarget() {
-  return <span id='about_target'>+</span>
+  return (
+    <div id='about_target_test_block'>
+      <span id='about_target'>+</span>
+      <p>TEST</p>
+      <p>{JSON.stringify(window.DeviceMotionEvent)}</p>
+    </div>
+  )
 }
