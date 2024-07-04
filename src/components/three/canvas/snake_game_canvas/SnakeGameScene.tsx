44168bc4808a708244746c18d822f90c319c420e
moco-xta <moco.xta@gmail.com>
2024-07-04 21:41:50 +0200

    AboutCanvas [MINECRAFT]: issue fixed cerating cubes

--- src/components/three/canvas/snake_game_canvas/SnakeGameScene.tsx
+++ src/components/three/canvas/snake_game_canvas/SnakeGameScene.tsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-import NewBoard from './Board'
-import Ground from './Ground'
-import Grid from './Grid'
+import useSnakeGameKeyboard from '@/hooks/useSnakeGameKeyboard'
 
-export default function SnakeGameScene() {
+import { AppDispatch, RootState } from '@/redux/store'
+import { setNextStep } from '@/redux/slice/snakeGameSlice'
+
+import Cell from './Cell'
+
+import { default as snakeGameConstants } from '@/constants/canvas/snakeGameConstants.json'
+
+export default function Board() {
+  const dispatch = useDispatch<AppDispatch>()
+  const snakeGameState = useSelector((state: RootState) => state.snakeGame)
+
+  const {} = useSnakeGameKeyboard()
+
+  const nextStep = setInterval(() => {
+    if (snakeGameState.isPlaying) {
+      dispatch(setNextStep())
+    }
+  }, snakeGameConstants.SNAKE_GAME.DEFAULT.SPEED)
+
+  useEffect(() => {
+    return () => clearInterval(nextStep)
+  }, [nextStep])
+
   return (
-    <>
-      <NewBoard />
-      <Ground />
-      {/* <Grid /> */}
-    </>
+    <group>
+      {snakeGameState.grid.map((cellsColumn, columnIndex) =>
+        cellsColumn.map((cell, rowIndex) => (
+          <Cell
+            key={`grid_cell_${columnIndex}_${rowIndex}`}
+            cell={cell}
+          />
+        )),
+      )}
+    </group>
   )
 }
