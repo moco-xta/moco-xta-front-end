2ccfa3ff8f02cf8b9de3551b0d7f1f2079151fbf
moco-xta <moco.xta@gmail.com>
2024-10-10 08:10:29 +0200

    Home: HomeCanvas + HomeScene (default cube)

--- src/components/slices/home_slice/index.tsx
+++ src/components/slices/home_slice/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+import HomeCanvas from '@/components/three/canvas/home_canvas/HomeCanvas'
+
 import './index.scss'
 
 export default function HomeSlice() {
   return (
-    <div>HomeSlice</div>
+    <section id='home_slice'>
+      <HomeCanvas />
+    </section>
   )
 }
---
+++ src/components/three/canvas/home_canvas/HomeCanvas.tsx
@@ -0,0 +1,27 @@
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+
+import HomeScene from './HomeScene'
+
+import { default as homeConstants } from '@/constants/canvas/homeConstants.json'
+
+export default function HomeCanvas() {
+  return (
+    <Canvas
+      dpr={homeConstants.CANVAS.DPR}
+      legacy={homeConstants.CANVAS.LEGACY}
+      shadows
+      gl={{
+        antialias: homeConstants.CANVAS.GL.ANTIALIAS,
+        alpha: homeConstants.CANVAS.GL.ALPHA,
+        powerPreference: homeConstants.CANVAS.GL.POWER_PREFERENCE,
+      }}
+    >
+      <OrbitControls />
+      <Suspense fallback={null}>
+        <HomeScene />
+      </Suspense>
+    </Canvas>
+  )
+}
