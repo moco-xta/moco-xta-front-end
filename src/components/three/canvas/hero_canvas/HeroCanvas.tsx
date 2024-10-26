30cd4cf71e10af177f752991e3af88311b4df819
moco-xta <moco.xta@gmail.com>
2024-10-26 19:36:43 +0200

    home: useLenis moved from HeroScene to HeroCanvas

--- src/components/three/canvas/hero_canvas/HeroCanvas.tsx
+++ src/components/three/canvas/hero_canvas/HeroCanvas.tsx
@@ -1,9 +1,10 @@
 import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
+import { useLenis } from 'lenis/react'
 import studio from '@theatre/studio'
 import extension from '@theatre/r3f/dist/extension'
 import { SheetProvider } from '@theatre/r3f'
-import { getProject } from '@theatre/core'
+import { getProject, val } from '@theatre/core'
 
 import Camera from './Camera'
 import Lights from './Lights'
@@ -16,6 +17,11 @@ studio.initialize()
 
 export default function HeroCanvas() {
   const sheet = getProject('Project TEST').sheet('Scene TEST')
+
+  useLenis(({ scroll }) => {
+    const sequenceLength = val(sheet!.sequence.pointer.length)
+    sheet!.sequence.position = (scroll / 1000) * sequenceLength
+  })
 
   return (
     <section
--- src/components/three/canvas/hero_canvas/HomeScene.tsx
+++ src/components/three/canvas/hero_canvas/HomeScene.tsx
@@ -1,18 +1,9 @@
-import React, { useEffect } from 'react'
-import { useLenis } from 'lenis/react'
-import { editable as e, useCurrentSheet } from '@theatre/r3f'
-import { val } from '@theatre/core'
+import React from 'react'
+import { editable as e } from '@theatre/r3f'
 
 import { Building001Wall } from '@/components/three/models/home/buildings/Building001Wall'
 
 export default function HomeScene() {
-  const sheet = useCurrentSheet()
-
-  useLenis(({ scroll }) => {
-    const sequenceLength = val(sheet!.sequence.pointer.length)
-    sheet!.sequence.position = (scroll / 1000) * sequenceLength
-  })
-
   return (
     <>
       <Building001Wall />
