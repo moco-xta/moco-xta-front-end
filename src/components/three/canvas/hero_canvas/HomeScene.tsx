d376921c0e05a0e96103850d67b0f58c4252d70a
moco-xta <moco.xta@gmail.com>
2024-11-05 11:12:43 +0100

    ExoMarket: new model + new diffuse texture

--- src/components/three/canvas/hero_canvas/HomeScene.tsx
+++ src/components/three/canvas/hero_canvas/HomeScene.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { editable as e } from '@theatre/r3f'
 
 import { Building001Wall } from '@/components/three/models/home/buildings/Building001Wall'
+import { ExoMarket } from '../../models/home/buildings/ExoMarket'
 
 export default function HomeScene() {
   return (
     <>
       <Building001Wall />
+      <ExoMarket />
       <e.mesh
         theatreKey='Cube'
         receiveShadow
