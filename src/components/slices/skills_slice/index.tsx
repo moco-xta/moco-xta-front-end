22f934bf95317c152edad08298b9568376bf54c9
moco-xta <moco.xta@gmail.com>
2024-11-15 18:27:20 +0100

    SkillsSlice: rfc + index.scss

--- src/components/slices/skills_slice/index.tsx
+++ src/components/slices/skills_slice/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
 
-import RubiksCubeCanvas from '@/components/three/canvas/rubiks_cube_canvas/RubiksCubeCanvas'
-import SkillCanvas from '@/components/three/canvas/skill_canvas/SkillCanvas'
-
 import './index.scss'
 
 export default function SkillsSlice() {
   return (
     <section id='skills_slice'>
-      <SkillCanvas />
-      <RubiksCubeCanvas />
+      Skills
     </section>
   )
 }
