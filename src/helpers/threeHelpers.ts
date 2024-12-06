fe463653ac7a4a8df5ee7eff3f38c67047e55f37
moco-xta <moco.xta@gmail.com>
2024-12-06 11:11:52 +0100

    /helpers: cleaned

--- src/helpers/threeHelpers.ts
+++ src/helpers/threeHelpers.ts
@@ -1,15 +1,55 @@
 import * as THREE from 'three'
 import { ThreeEvent } from '@react-three/fiber'
 
-export function generateRandomRgbGrey(min: number, max: number) {
-  const value = Math.round(Math.random() * (max - min) + min)
-  return new THREE.Color(`rgb(${value}, ${value}, ${value})`)
+import { getRandomInt, randomPositiveOrNegative } from './mathHelpers'
+
+interface CoordinatesInterface {
+  x: number
+  y: number
+  z: number
 }
 
-export function generateRandomHexGrey(min: number, max: number) {
-  const decimalValue = Math.round(Math.random() * (max - min) + min)
-  var hexadecimalValue = decimalValue.toString(16)
-  return new THREE.Color('#' + hexadecimalValue + hexadecimalValue + hexadecimalValue)
+interface SelectedAxisInterface {
+  x?: boolean
+  y?: boolean
+  z?: boolean
+}
+
+export interface RandomRangePositionInterface {
+  min: number | CoordinatesInterface
+  max: number | CoordinatesInterface
+  decimal?: number
+}
+
+export const setRandomPosition = ({ min, max, decimal = 1 }: RandomRangePositionInterface) => {
+  return new THREE.Vector3(
+    randomPositiveOrNegative() *
+      getRandomInt({
+        min: typeof min !== 'object' ? min : min.x,
+        max: typeof max !== 'object' ? max : max.x,
+        decimal: decimal,
+      }),
+    randomPositiveOrNegative() *
+      getRandomInt({
+        min: typeof min !== 'object' ? min : min.y,
+        max: typeof max !== 'object' ? max : max.y,
+        decimal: decimal,
+      }),
+    randomPositiveOrNegative() *
+      getRandomInt({
+        min: typeof min !== 'object' ? min : min.z,
+        max: typeof max !== 'object' ? max : max.z,
+        decimal: decimal,
+      }),
+  )
+}
+
+export const setRandomRotation = ({ x, y, z }: SelectedAxisInterface) => {
+  return new THREE.Euler(
+    x ? THREE.MathUtils.degToRad(getRandomInt({ min: 0, max: 360 })) : 0,
+    y ? THREE.MathUtils.degToRad(getRandomInt({ min: 0, max: 360 })) : 0,
+    z ? THREE.MathUtils.degToRad(getRandomInt({ min: 0, max: 360 })) : 0,
+  )
 }
 
 export function getUvMousePositionOnMesh(event: ThreeEvent<PointerEvent>, name: string) {
@@ -26,4 +66,15 @@ export function getUvMousePositionOnMesh(event: ThreeEvent<PointerEvent>, name: 
     x: (100 * pointCoordinates.x) / max.x,
     y: (100 * pointCoordinates.y) / max.y,
   }
+}
+
+export function generateRandomRgbGrey(min: number, max: number) {
+  const value = Math.round(Math.random() * (max - min) + min)
+  return new THREE.Color(`rgb(${value}, ${value}, ${value})`)
+}
+
+export function generateRandomHexGrey(min: number, max: number) {
+  const decimalValue = Math.round(Math.random() * (max - min) + min)
+  var hexadecimalValue = decimalValue.toString(16)
+  return new THREE.Color('#' + hexadecimalValue + hexadecimalValue + hexadecimalValue)
 }
