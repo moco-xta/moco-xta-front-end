e6918b8509bd21c876b8e1d4b62c5644311a0be4
moco-xta <moco.xta@gmail.com>
2025-01-10 23:51:01 +0100

    @/helpers: issue fixed ('ReferenceError: document is not defined')

--- src/helpers/cookiesHelpers.ts
+++ src/helpers/cookiesHelpers.ts
@@ -1,10 +1,13 @@
 export function getCookieByName(name: string) {
-  const cookie: {
-    [key: string]: string
-  } = {}
-  document.cookie.split(';').forEach(function (el) {
-    const [key, value] = el.split('=')
-    cookie[key.trim()] = value
-  })
-  return cookie[name]
+  if (typeof document === 'undefined') return undefined
+
+  const cookies = document.cookie.split(';')
+  for (const cookie of cookies) {
+    const [key, value] = cookie.split('=')
+    if (key.trim() === name) {
+      return value
+    }
+  }
+
+  return undefined
 }
