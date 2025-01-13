bdeb7a108dd864e53c84e72e010f79398457065a
moco-xta <moco.xta@gmail.com>
2025-01-13 11:17:29 +0100

    LogoMoco: close Menu onClick

--- src/components/layout/menu/header/logo_moco/index.tsx
+++ src/components/layout/menu/header/logo_moco/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import { gsap } from 'gsap'
 import { useGSAP } from '@gsap/react'
 
 import type { TLogoMoco } from '@/types/components/types'
 
 import { Link } from '@/i18n/routing'
+
+import { AppDispatch } from '@/redux/store'
+import { setLocaleSwitcherIsOpen, setMenuIsOpen } from '@/redux/slices/appStateSlice'
 
 import { LogoMocoCanvas } from '@/components/three/canvas'
 
@@ -14,12 +18,22 @@ import { logoMocoData } from '@/data/menu/header/logo_moco/three/logoMocoData'
 import './index.scss'
 
 export default function LogoMoco({ isMain = false }: TLogoMoco) {
+  const dispatch = useDispatch<AppDispatch>()
+
+  const handleOnClickLogoMoco = () => {
+    dispatch(setMenuIsOpen(false))
+    dispatch(setLocaleSwitcherIsOpen(false))
+  }
+
   useGSAP(() => {
     if (isMain) gsap.from(logoMocoData.animations.selector, logoMocoData.animations.params)
   })
 
   return (
-    <div id={`${isMain ? 'main_' : ''}logo_moco`}>
+    <div
+      id={`${isMain ? 'main_' : ''}logo_moco`}
+      onClick={handleOnClickLogoMoco}
+    >
       <Link href={routesData.find((route) => route.translationKey === 'HOME')!.path}>
         <LogoMocoCanvas />
       </Link>
