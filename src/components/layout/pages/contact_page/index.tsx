7813d641c4c3ab7a497e11a33297fc7f23c82620
moco-xta <moco.xta@gmail.com>
2025-02-13 23:46:00 +0100

    Contact: good base design

--- src/components/layout/pages/contact_page/index.tsx
+++ src/components/layout/pages/contact_page/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useTranslations } from 'next-intl'
+import { FaPhoneAlt } from 'react-icons/fa'
+import { MdEmail } from 'react-icons/md'
 
 import { GSAPTimelineProvider } from '@/contexts/GsapTimelineContext'
 
@@ -8,13 +11,35 @@ import SocialsCanvas from '@/components/three/canvas/socials_canvas/SocialsCanva
 import { timelineDefaultValues } from '@/data/contact/timelineData'
 
 import './index.scss'
+import { SectionTitle } from '../../titles'
 
-export default function index() {
+export default function Index() {
+  const t = useTranslations('CONTACT')
+
   return (
     <div id='contact_page'>
-      <section id='contact_section'>
-        <SocialsCanvas className='contact_socials_canvas' />
-      </section>
+      <div id='contact_container'>
+        <section id='contact_section'>
+          <SectionTitle title={t('CONTACT')} />
+          <div id='contact_content'>
+            <p>{t('INTRODUCTION')}</p>
+            <SocialsCanvas className='contact_socials_canvas' />
+            <div>
+              <h3>Contact</h3>
+              <div>
+                <p id='email_address'>
+                  <MdEmail />
+                  moco.xta@gmail.com
+                </p>
+                <p id='phone_number'>
+                  <FaPhoneAlt />
+                  +33 6 77 77 77 77
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
       <GSAPTimelineProvider {...timelineDefaultValues}>
         <ContactCanvas />
       </GSAPTimelineProvider>
