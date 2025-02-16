fc0e5ff66f18aa755fb19221de27864e33c82480
moco-xta <moco.xta@gmail.com>
2025-02-16 16:37:22 +0100

    npm i formik --save

--- src/components/layout/pages/colors_page/index.tsx
+++ src/components/layout/pages/colors_page/index.tsx
@@ -1,24 +1,32 @@
 import React from 'react'
+import { useTranslations } from 'next-intl'
 
 import { PageTitle } from '../../titles'
-import Color from './color'
+// import Color from './color'
 
-import { colorsData } from '@/data/colors/colorsData'
+// import { colorsData } from '@/data/colors/colorsData'
 
 import './index.scss'
 
 export default function ColorPage() {
+  const t = useTranslations('COLORS')
+
   return (
     <div id='color_page'>
       <div id='color_content'>
         <PageTitle translationPath={'COLORS.TITLE'} />
-        {colorsData.map((palette, index) => (
-          <div className='palette_container'>
+        <div id='colors_introduction'>
+          {t.rich('INTRODUCTION', {
+            p: (chunk) => <p>{chunk}</p>,
+          })}
+        </div>
+        {/* {colorsData.map((palette, index) => (
+          <div key={`palette_${index}`} className='palette_container'>
             {palette.map((color) => (
-              <Color color={color} />
+              <Color key={`color_${color}`} color={color} />
             ))}
           </div>
-        ))}
+        ))} */}
       </div>
     </div>
   )
