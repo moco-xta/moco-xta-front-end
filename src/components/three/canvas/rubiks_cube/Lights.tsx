d9b9633d2320d32282f959a430aef73d6a5ee0f0
moco-xta <moco.xta@gmail.com>
2025-02-17 10:00:06 +0100

    npm run build

--- src/components/three/canvas/rubiks_cube/Lights.tsx
+++ src/components/three/canvas/rubiks_cube/Lights.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Environment } from '@react-three/drei'
 
-import { lightsData } from '@/data/skills/rubiks_cube/three/lightsData'
+// import { lightsData } from '@/data/skills/rubiks_cube/three/lightsData'
 
 import { default as imgConstants } from '@/constants/assets/imgConstants.json'
 
 export default function Lights() {
   return (
     <>
-    <ambientLight intensity={2} />
+      <ambientLight intensity={2} />
       <Environment
         files={imgConstants.EXR.STUDIO_001}
         // environmentIntensity={0.75}
