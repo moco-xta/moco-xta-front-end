b915e09179c4e2a14210d3502df6e9340c2f234c
moco-xta <moco.xta@gmail.com>
2025-02-25 10:44:33 +0100

    Contact: powder effect removed

--- src/data/contact/three/cameraData.ts
+++ src/data/contact/three/cameraData.ts
@@ -7,7 +7,7 @@ import { getDegreeEuler } from '@/helpers/threeHelpers'
 export const cameraDefaultValues: TCameraDefaultValues = {
   camera: {
     makeDefault: true,
-    position: new THREE.Vector3(0, 0, 10),
+    position: new THREE.Vector3(4, -1, 10),
     rotation: getDegreeEuler({ x: 0, y: 0, z: 0 }),
     fov: 50,
     near: 0.01,
