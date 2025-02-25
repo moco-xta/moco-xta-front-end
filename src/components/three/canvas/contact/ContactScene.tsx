34bcd320686edc655fcefc0d416762e30446a36f
moco-xta <moco.xta@gmail.com>
2025-02-25 10:38:32 +0100

    Contact: extras removed

--- src/components/three/canvas/contact/ContactScene.tsx
+++ src/components/three/canvas/contact/ContactScene.tsx
@@ -2,23 +2,23 @@ import React from 'react'
 import * as THREE from 'three'
 
 import BustComponent from './bust/BustComponent'
-import HeartComponent from './HeartComponent'
+// import HeartComponent from './HeartComponent'
 import LogoGithubComponent from './socials_logos/LogoGithubComponent'
 import LogoInstagramComponent from './socials_logos/LogoInstagramComponent'
 import LogoLinkedInComponent from './socials_logos/LogoLinkedInComponent'
 import LogoPinterestComponent from './socials_logos/LogoPinterestComponent'
 import LogoXComponent from './socials_logos/LogoXComponent'
 import { getDegreeEuler } from '@/helpers/threeHelpers'
-import LogoAthleticoNacionalComponent from './LogoAthleticoNacionalComponent'
-import { Gobelino } from '../../models/contact/Gobelino'
+// import LogoAthleticoNacionalComponent from './LogoAthleticoNacionalComponent'
+// import { Gobelino } from '../../models/contact/Gobelino'
 
 export default function ContactScene() {
   return (
     <group>
       <BustComponent />
-      <HeartComponent />
-      <LogoAthleticoNacionalComponent />
-      <Gobelino position={new THREE.Vector3(0, 0, -1)} />
+      {/* <HeartComponent /> */}
+      {/* <LogoAthleticoNacionalComponent /> */}
+      {/* <Gobelino position={new THREE.Vector3(0, 0, -1)} /> */}
       <group
         position={new THREE.Vector3(3.5, 0, 0)}
         rotation={getDegreeEuler({ y: -25 })}
