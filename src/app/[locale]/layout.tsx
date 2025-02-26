f1a3ce2f7a28c16dd989175d2921fe4ecfafd35d
moco-xta <moco.xta@gmail.com>
2025-02-26 07:22:28 +0100

    GoogleAnalytics: layout.tsx updated

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -24,7 +24,7 @@ export default function LocaleLayout({
   }
 }>) {
   const translations = useMessages()
-  const gaId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_KEY
+  // const gaId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_KEY
 
   return (
     <html lang={locale}>
@@ -52,7 +52,7 @@ export default function LocaleLayout({
           </ReduxProvider>
         </body>
       </ReactLenis>
-      <GoogleAnalytics gaId={gaId!} />
+      <GoogleAnalytics gaId="G-TN9SNWP2HB" />
     </html>
   )
 }
