5616ec8e55fb7343dbbb2c801a005def77bb8a98
moco-xta <moco.xta@gmail.com>
2025-03-17 20:45:57 +0100

    Header: restructured (splitted in 2 blocks)

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -16,12 +16,18 @@ import './index.scss'
 export default function Header() {
   return (
     <>
-      <header className='header_zIndex'>
+      <header
+        id='header_logo'
+        className='header_zIndex'
+      >
         <LogoMocoCanvas logoMocoMaterial={getMatteMaterial(logoMocoData.material.color)} />
-        <div id='header_buttons'>
-          <LocaleSwitcherButton />
-          <MenuButton />
-        </div>
+      </header>
+      <header
+        id='header_buttons'
+        className='header_zIndex'
+      >
+        <LocaleSwitcherButton />
+        <MenuButton />
       </header>
       <ButtonsContents />
     </>
