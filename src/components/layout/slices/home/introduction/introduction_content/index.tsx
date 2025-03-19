633b7b6d21fabb7ff5a3ee90e921609820bae896
moco-xta <moco.xta@gmail.com>
2025-03-19 10:10:39 +0100

    Home: IntroductionContext introduction text set up done for animation

--- src/components/layout/slices/home/introduction/introduction_content/index.tsx
+++ src/components/layout/slices/home/introduction/introduction_content/index.tsx
@@ -10,12 +10,16 @@ export type TIntroductionContent = {
   introductionSectionRef: RefObject<HTMLElement>
 }
 
-export type TextRevealed = {
+export type TTextRevealed = {
   id: string
   text: string
 }
 
-function TextRevealed({ id, text }: TextRevealed) {
+export type TLineRevealed = {
+  text: string
+}
+
+function TextRevealed({ id, text }: TTextRevealed) {
   return (
     <div className='text_revealed'>
       <span
@@ -25,6 +29,14 @@ function TextRevealed({ id, text }: TextRevealed) {
         {text}
         &nbsp;
       </span>
+    </div>
+  )
+}
+
+function LineRevealed({ text }: TLineRevealed) {
+  return (
+    <div className='introduction_line_wrapper'>
+      <p className='introduction_line gradient-text'>{text}</p>
     </div>
   )
 }
@@ -77,38 +89,60 @@ export default function IntroductionContent({ introductionSectionRef }: TIntrodu
         duration: 0.5,
         ease: 'power1.out',
       })
+
+    const animationFn = gsap.to('.introduction_line', {
+      scrollTrigger: {
+        trigger: introductionSectionRef.current,
+        start: 'top center',
+        markers: true,
+      },
+      yPercent: -100,
+      rotateZ: '-15deg',
+      duration: 2.5,
+      ease: 'power1.out',
+      stagger: 0.2,
+    })
+    return () => animationFn.kill()
   })
 
   return (
-    <div
-      id='introduction_content'
-      className={`${helveticaMediumFont.className}`}
-    >
-      <TextRevealed
-        id={'hi_text'}
-        text={t('HI')}
-      />
-      <TextRevealed
-        id={'im_text'}
-        text={t('I_M')}
-      />
-      <TextRevealed
-        id={'federico_text'}
-        text={'Federico'}
-      />
-      <br />
-      <TextRevealed
-        id={'a_text'}
-        text={t('A')}
-      />
-      <TextRevealed
-        id={'front_end_text'}
-        text={t('FRONT_END')}
-      />
-      <TextRevealed
-        id={'developer_text'}
-        text={t('DEVELOPER')}
-      />
+    <div id='introduction_content'>
+      <div className={`${helveticaMediumFont.className}`}>
+        <TextRevealed
+          id={'hi_text'}
+          text={t('TITLE.HI')}
+        />
+        <TextRevealed
+          id={'im_text'}
+          text={t('TITLE.I_M')}
+        />
+        <TextRevealed
+          id={'federico_text'}
+          text={'Federico'}
+        />
+        <br />
+        <TextRevealed
+          id={'a_text'}
+          text={t('TITLE.A')}
+        />
+        <TextRevealed
+          id={'front_end_text'}
+          text={t('TITLE.FRONT_END')}
+        />
+        <TextRevealed
+          id={'developer_text'}
+          text={t('TITLE.DEVELOPER')}
+        />
+      </div>
+
+      <div>
+        <LineRevealed text={t('INTRODUCTION.LINE_1')} />
+        <LineRevealed text={t('INTRODUCTION.LINE_2')} />
+        <LineRevealed text={t('INTRODUCTION.LINE_3')} />
+        <LineRevealed text={t('INTRODUCTION.LINE_4')} />
+        <LineRevealed text={t('INTRODUCTION.LINE_5')} />
+        <LineRevealed text={t('INTRODUCTION.LINE_6')} />
+      </div>
     </div>
   )
 }
