93e62391f11805eda979577ef00b72509f4b7e31
moco-xta <moco.xta@gmail.com>
2025-03-19 15:57:05 +0100

    Home > MainSkills: section + title with translations

--- src/components/layout/slices/home/main_skills/index.tsx
+++ src/components/layout/slices/home/main_skills/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useTranslations } from 'next-intl'
 
 import './index.scss'
 
 export default function MainSkills() {
+  const t = useTranslations('HOME.MAIN_SKILLS')
+
   return (
-    <section id='main_skills_section'>MainSkills</section>
+    <section id='main_skills_section'>
+      <p className='section_title'>{t('SECTION_TITLE')}</p>
+    </section>
   )
 }
