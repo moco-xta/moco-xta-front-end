2659e9c452765cd721b5b79d590a4d3a2468d1de
moco-xta <moco.xta@gmail.com>
2025-03-20 15:57:55 +0100

    Skills > SkillsSlice > SkillsDétails: description added NO design

--- src/components/layout/slices/skills/skills/skills_details/index.tsx
+++ src/components/layout/slices/skills/skills/skills_details/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import { useTranslations } from 'next-intl'
+
+import { usePageContext } from '@/contexts/PageContext'
 
 import SkillsLogosCanvas from '@/components/three/canvas/skills_logos/SkillsLogosCanvas'
 
 import './index.scss'
 
 export default function SkillsDetails() {
+  const t = useTranslations()
+  
+  const { currentSection, currentParagraph } = usePageContext()
+
   return (
     <div>
       <SkillsLogosCanvas />
+      <p>{t(`SKILLS.SECTIONS.${currentSection.translationKey}.PARAGRAPHS.${currentParagraph.translationKey}.PARAGRAPH_DESCRIPTION`)}</p>
     </div>
   )
 }
