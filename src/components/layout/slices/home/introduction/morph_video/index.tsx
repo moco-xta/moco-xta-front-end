011460fd3bb9590aa560ab1f58c9780c9e6b564e
moco-xta <moco.xta@gmail.com>
2025-03-23 11:16:57 +0100

    Home > Introduction: MorphVideo work on it BUT will continue at the end if time and video DONE

--- src/components/layout/slices/home/introduction/morph_video/index.tsx
+++ src/components/layout/slices/home/introduction/morph_video/index.tsx
@@ -2,6 +2,8 @@ import { RefObject, useEffect, useRef } from 'react'
 import anime from 'animejs'
 import gsap from 'gsap'
 
+import './index.scss'
+
 export type TMorphVideo = {
   introductionSectionRef: RefObject<HTMLElement>
 }
@@ -13,58 +15,64 @@ const MorphVideo = ({ introductionSectionRef }: TMorphVideo) => {
   useEffect(() => {
     if (typeof window === 'undefined' || !pathRef.current) return
 
-    // Anime.js morph animation
     const morphAnimation = anime({
       targets: pathRef.current,
       d: [
-        { value: 'M 10,30 50,30 50,70 10,70 Z' }, // Square
-        { value: 'M 30,10 70,10 70,50 30,50 Z' }, // Diamond
-        { value: 'M 20,20 80,20 80,80 20,80 Z' }, // Rectangle
-        {
+        { value: 'M 2.8958327,0.24999949 H 82.27083 c 1.465792,0 2.645834,1.18004161 2.645834,2.64583321 V 45.229166 c 0,1.465792 -1.180042,2.645833 -2.645834,2.645833 H 2.8958327 c -1.4657916,0 -2.64583321,-1.180041 -2.64583321,-2.645833 V 2.8958327 c 0,-1.4657916 1.18004161,-2.64583321 2.64583321,-2.64583321 z' },
+        // { value: 'M 105.4778,5.2366312 C 102.55703,4.985178 2.8959473,0.25011393 2.8959473,0.25011393 1.4545255,-0.01605136 0.25011393,1.4301571 0.25011393,2.8959473 V 21.416781 c 0,1.46579 1.20441157,2.379667 2.64583337,2.645833 L 105.4778,76.674131 206.62511,119.31261 c 2.92079,0.25145 5.29167,-2.36008 5.29167,-5.29166 V 5.5417806 c 0,-2.9315803 -2.37088,-5.04021292 -5.29167,-5.29166667 z' },
+        { value: 'M 2.8958327,0.24999949 H 82.27083 c 1.465792,0 2.645834,1.18004161 2.645834,2.64583321 V 45.229166 c 0,1.465792 -1.180042,2.645833 -2.645834,2.645833 H 2.8958327 c -1.4657916,0 -2.64583321,-1.180041 -2.64583321,-2.645833 V 2.8958327 c 0,-1.4657916 1.18004161,-2.64583321 2.64583321,-2.64583321 z' },
+        /* { value: 'M 5.541666,0.24999949 H 206.625 c 2.93159,0 5.29167,2.36008331 5.29167,5.29166651 V 114.02083 c 0,2.93159 -2.36008,5.29167 -5.29167,5.29167 H 5.541666 c -2.9315832,0 -5.29166651,-2.36008 -5.29166651,-5.29167 V 5.541666 c 0,-2.9315832 2.36008331,-5.29166651 5.29166651,-5.29166651 z'} */
+        /* {
           value:
-            'M 32.400619,54.148975 H 185.52682 c 6.63902,0 11.98379,5.344771 11.98379,11.983791 v 81.667304 c 0,6.63902 -5.34477,11.98379 -11.98379,11.98379 H 32.400619 c -6.63902,0 -11.983791,-5.34477 -11.983791,-11.98379 V 66.132766 c 0,-6.63902 5.344771,-11.983791 11.983791,-11.983791 z',
-        }, // Complex shape
+            'M 5.541666,0.24999949 H 418.29168 c 2.93158,0 5.29166,2.36008331 5.29166,5.29166651 V 233.08333 c 0,2.93159 -2.36008,5.29167 -5.29166,5.29167 H 5.541666 c -2.9315832,0 -5.29166651,-2.36008 -5.29166651,-5.29167 V 5.541666 c 0,-2.9315832 2.36008331,-5.29166651 5.29166651,-5.29166651 z',
+        } */
       ],
       easing: 'linear',
-      duration: 1, // Set duration to 1ms since GSAP will control it
-      autoplay: false, // Disable autoplay
+      duration: 1,
+      autoplay: false,
     })
 
-    // GSAP ScrollTrigger
     gsap.to(morphAnimation, {
-      progress: 1, // Animate from 0 to 100%
+      progress: 1,
       ease: 'none',
       scrollTrigger: {
         trigger: '.container',
-        start: 'top bottom', // Start animation when the element enters the viewport
-        end: 'top top', // End when it reaches the top
-        scrub: 1, // Smooth scrolling effect
+        start: 'top center',
+        end: 'top top',
+        scrub: 1,
+        markers: true,
       },
       onUpdate: () => {
-        morphAnimation.seek(morphAnimation.duration * morphAnimation.progress) // Sync anime.js
+        morphAnimation.seek(morphAnimation.duration * morphAnimation.progress)
       },
     })
-
-    // Handle video autoplay restrictions
+  
     if (videoRef.current) {
       videoRef.current.play().catch(() => {
-        const playButton = document.createElement('button')
-        playButton.innerHTML = 'Play Video'
-        playButton.style.position = 'absolute'
-        playButton.style.top = '20px'
-        playButton.style.left = '20px'
-        playButton.onclick = () => videoRef.current?.play()
-        document.querySelector('.container')?.appendChild(playButton)
-      })
+        const playButton = document.createElement('button');
+        playButton.innerHTML = 'Play Video';
+        Object.assign(playButton.style, {
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          padding: '10px 20px',
+          cursor: 'pointer',
+        });
+        playButton.onclick = () => {
+          videoRef.current?.play();
+          playButton.remove();
+        };
+        document.getElementById('morph_video_container')?.appendChild(playButton);
+      });
     }
   }, [])
 
   return (
     <div
+      id='morph_video_container'
       className='container'
-      style={{ position: 'relative', width: '100vw', height: '100vw' }}
     >
-      {/* Video element with SVG clip-path */}
       <video
         ref={videoRef}
         muted
@@ -75,8 +83,9 @@ const MorphVideo = ({ introductionSectionRef }: TMorphVideo) => {
           width: '100%',
           height: '100%',
           clipPath: 'url(#morphClip)',
-          objectFit: 'cover',
+          objectFit: 'fill',
         }}
+        controls
       >
         <source
           src='/videos/test.mp4'
@@ -84,7 +93,6 @@ const MorphVideo = ({ introductionSectionRef }: TMorphVideo) => {
         />
       </video>
 
-      {/* SVG definition for clip-path */}
       <svg style={{ position: 'absolute', width: 0, height: 0 }}>
         <defs>
           <clipPath
@@ -93,8 +101,8 @@ const MorphVideo = ({ introductionSectionRef }: TMorphVideo) => {
           >
             <path
               ref={pathRef}
-              d='M 0.2,0.3 0.5,0.3 0.5,0.7 0.2,0.7 Z' // Initial shape
-              transform='scale(0.01)' // Convert viewBox 100x100 to 0-1 coordinates
+              d='M 2.8958327,0.24999949 H 39.937498 c 1.465792,0 2.645834,1.18004161 2.645834,2.64583321 V 21.416666 c 0,1.465792 -1.180042,2.645833 -2.645834,2.645833 H 2.8958327 c -1.4657916,0 -2.64583321,-1.180041 -2.64583321,-2.645833 V 2.8958327 c 0,-1.4657916 1.18004161,-2.64583321 2.64583321,-2.64583321 z'
+              transform='scale(0.01)'
             />
           </clipPath>
         </defs>
